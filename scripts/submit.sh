017bf5d18e2fe91ed97e54bd046279b0649afe09
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2018-03-26 02:23:57 +0000

    make it so that submit.sh can take extra args for each specific submit script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+BINPROCESS=$gmsim/workflow/scripts
 #script needs one arg(emod3d,post-emod, hf, or bb)
 if [[ $# -lt 1 ]];then
     echo "please provide the jobs you want to submit (emod3d,post_emod3d, hf, or bb)."
@@ -7,27 +7,35 @@ if [[ $# -lt 1 ]];then
 fi
 job=$1
 #use ls to test if the desired submit script exsist
-ls $gmsim/workflow/scripts/submit_$job.py 2>1 1>/dev/null
+ls $BINPROCESS/submit_$job.py 2>&1 1>/dev/null
 #
 if [[ $? -ne 0 ]];then
     echo "Error: cannot find script for $job"
     exit 1
 fi
 
-#get $BINPROCESS from machine_env.sh
-source machine_env.sh
-
+#a quick hack to store all the extra args for the script
+count=0
+for args in $@
+do
+    #ignore the first arg
+    if [[ $count == 0 ]];then
+        count=$count+1
+        continue
+    fi
+    additional_args="$additional_args $args"
+done
 
 #submit emod3d
 if [[ $job = emod3d ]]; then
     echo submit_emod3d
-    python $BINPROCESS/submit_emod3d.py
+    python $BINPROCESS/submit_emod3d.py $additional_args
 elif [[ $job = post_emod3d ]]; then
     echo submit_post_emod3d
-    python $BINPROCESS/submit_post_emod3d.py
+    python $BINPROCESS/submit_post_emod3d.py $additional_args
 elif [[ $job = hf ]]; then
     echo submit_hf
-    python $BINPROCESS/submit_hf.py
+    python $BINPROCESS/submit_hf.py $additional_args
 elif [[ $jon = bb ]]; then
-    python $BINPROCESS/submit_bb.py
+    python $BINPROCESS/submit_bb.py $additional_args
 fi
