98914830226a5fb95d02cba1aa81fe08d6750e80
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2018-04-04 04:12:43 +0000

    ch_report.sh now takes an arg as the period of days to report

--- scripts/CH_report.sh
+++ scripts/CH_report.sh
@@ -4,8 +4,17 @@ year=`date +%Y`
 month=`date +%m`
 day=`date +%d`
 
-period=`echo 7 | bc`
+if [[ $# -lt 1 ]]; then
+    echo "please provide a period number( e.g 7 for 7 days)"
+    exit 1
+fi 
+period=`echo $1 | bc`
 
+if (( $period > 28 ));then
+    echo "this script is not designed to be used for more than 28 days, please manually run the command if you desire."
+    echo "example: sreport -t Hours cluster AccountUtilizationByUser Accounts=nesi00213 start=2017-01-01"
+    exit 1
+fi
 
 #check for underflow of day (when executed at start of the month)
 list_31days='03 05 07 08 10 12'
@@ -46,6 +55,9 @@ if (( $start_day <  1)); then
             start_day=`echo 30+$day-$period | bc`
         fi
     fi
+fi
+if (( $start_month < 10 )); then
+    start_month=0$start_month
 fi
 start_date=$start_year-$start_month-$start_day
 end_date=$year-$month-$day
