4a4a2069b4bb4b1e7cf6cc9606b2d5363d60a2bf
ttgenius <melodyzyz@gmail.com>
2018-10-12 04:11:28 +0000

    onoff so no need clean up

--- find/find_params.py
+++ find/find_params.py
@@ -14,7 +14,7 @@ def get_scripts_imports(git_folder, params_dir):
         script, imports = line.split(':')
         if not imports.startswith("#"):
             params_name = imports.split('from')[1].split('import')[0].strip()
-       
+
             if '.py' not in params_name:
                 params_name += '.py'
 
@@ -41,8 +41,8 @@ def get_all_params_dict(params_dir):
     params_template_list = glob.glob1(params_dir, 'params*.py')
     params_dict = {}
     for params_template in params_template_list:
-      #  print(params_template)
-        params = load_py_cfg(os.path.join(params_dir,params_template))
+        #  print(params_template)
+        params = load_py_cfg(os.path.join(params_dir, params_template))
         params_dict[params_template] = [[k, 0] for k in params.keys()]
     return params_dict
 
@@ -57,7 +57,7 @@ def load_py_cfg(f_path):
 def get_scripts_context(git_dir, script, imports_dict, params_dict):
     log = open("params_usage_{}.txt".format(git_dir.split('/')[-1]), 'a')
     imported_params = imports_dict[script]
-    log.write("@{}{}\n".format(script,'-'*150))
+    log.write("@{}{}\n".format(script, '-' * 150))
     for params_file in imported_params:
         for i in range(len(params_dict[params_file])):
             param, _ = params_dict[params_file][i]
@@ -67,61 +67,32 @@ def get_scripts_context(git_dir, script, imports_dict, params_dict):
                 if output != '':
                     log.write("{}, {}\n".format(params_file, param))
                     params_dict[params_file][i][1] = 1
-        #        print("found", params_file,params_dict[params_file][i][0])
+                    #        print("found", params_file,params_dict[params_file][i][0])
     log.write('\n\n')
     log.close()
     return params_dict
 
 
+def write_param_dict(outname, header, param_dict):
+    with open(outname, 'w') as c:
+        cw = csv.writer(c, delimiter=',', quotechar='|')
+        cw.writerow(header)
+        for k, v in param_dict.items():
+            for pa, bol in v:
+                if bol == 0 and not pa.startswith('__'):
+                    cw.writerow([k, pa])
 
 
-
-
-#TODO gs of >=2 items not working properly
-#TODO write params_dict to file
-#TODO params in qcore is: import params.....
+# TODO params in qcore is: import params.....
 p = get_all_params_dict('.')
 
-g = '/home/melody.zhu/slurm_gm_workflow'
-d = get_scripts_imports(g,'.')
-print("Afddsafs",d.keys())
-for s in d.keys():
-    print(s)
-    p = get_scripts_context(g, s, d, p)
-
-gg = '/home/melody.zhu/qcore'
-dd = get_scripts_imports(gg,'.')
-print("Afddsafs",dd.keys())
-for s in dd.keys():
-    print(s)
-    p = get_scripts_context(gg, s, dd, p)
-
-
-ggg = '/home/melody.zhu/visualization'
-ddd = get_scripts_imports(ggg,'.')
-print("Afddsafs",ddd.keys())
-for s in ddd.keys():
-    print(s)
-    p = get_scripts_context(ggg, s, ddd, p)
-
-
-
-with open("params_used.csv", 'w') as c:
-    cw = csv.writer(c,  delimiter=',',quotechar='|')
-    cw.writerow(['params_script', 'used_param'])
-    for k, v in p.items():
-        print("Afdsafads",k,v)
-        for pa, bol in v:
-            if bol == 1 and not pa.startswith('__'):
-                cw.writerow([k, pa])
-
-
-with open("params_unused.csv", 'w') as c:
-    cw = csv.writer(c, delimiter=',',quotechar='|')
-    cw.writerow(['params_script', 'unused_param'])
-    for k, v in p.items():
-        for pa, bol in v:
-            if bol == 0 and not pa.startswith('__'):
-                cw.writerow([k, pa])
-
+gs = ['/home/melody.zhu/slurm_gm_workflow', '/home/melody.zhu/qcore', '/home/melody.zhu/visualization']
+for g in gs:
+    d = get_scripts_imports(g, '.')
+    print("Afddsafs", d.keys())
+    for s in d.keys():
+        print(s)
+        p = get_scripts_context(g, s, d, p)
+write_param_dict("params_used.csv", ['params_script', 'used_param'], p)
+write_param_dict("params_unused.csv", ['params_script', 'unused_param'], p)
 
