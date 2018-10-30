cf46d2f2d4167b9758c5c70eb28d92bee1fa1653
ttgenius <melodyzyz@gmail.com>
2018-10-30 03:00:32 +0000

    ordereddict for dump yaml

--- find/dump_yaml.py
+++ find/dump_yaml.py
@@ -1,10 +1,12 @@
 import yaml
-import imp
+from collections import OrderedDict
 
 
 def dump_yaml(input_dict, output_name):
     with open(output_name, 'w') as yaml_file:
+        yaml.add_representer(OrderedDict, lambda dumper, data: dumper.represent_mapping('tag:yaml.org,2002:map', data.items()))
         yaml.dump(input_dict, stream=yaml_file, default_flow_style=False)
+
 
 def create_dict(input_file):
     with open(input_file, 'r') as f:
@@ -13,13 +15,16 @@ def create_dict(input_file):
     for line in lines[:-6]:
         if line != '\n' and line != '' and line and not line.startswith("import") and not line.startswith("#") and not line.startswith("__") and not line.startswith("from"):
             print(line)
-            p, v = line.split('=')[0].replace("'",'').strip(),line.split('=')[1].split("#")[0].replace("'",'').strip()
-            d[p] = v
+            if line.startswith('@'):
+                k = line.strip().split('@')[-1]
+                d[k]=OrderedDict()
+            else:
+            	p, v = line.split('=')[0].replace("'",'').strip(),line.split('=')[1].split("#")[0].replace("'",'').strip()
+            	d[k][p] = v
     print(d)
     return d
   
 
-d = load_py_cfg('/home/melody.zhu/params.py')
 d= create_dict('/home/melody.zhu/params.py')
 d.update(create_dict('/home/melody.zhu/params_base.py'))
 dump_yaml(d, "params.yaml")
