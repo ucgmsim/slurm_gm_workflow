8325fd0326b6ab45677a52e6d416d68db05db710
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2018-11-14 03:03:09 +0000

    fixed logic error for hf_sim seed when -i

--- scripts/hf_sim.py
+++ scripts/hf_sim.py
@@ -136,7 +136,7 @@ if is_master:
 args = comm.bcast(args, root = master)
 
 #change seed for each rank if 'i' is not set
-if args.independent == True:
+if args.independent != True:
     args.seed += rank
 print ("Rank %d seed: %d" %(rank, args.seed))
 
