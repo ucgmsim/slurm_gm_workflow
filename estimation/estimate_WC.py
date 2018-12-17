2e6173ea72a6dfd237a671af6eb3a861de0e36c7
claudio <claudio.schill@canterbury.ac.nz>
2018-12-17 14:45:24 +1300

    Added estimate functions for HF and BB

--- estimation/estimate_WC.py
+++ estimation/estimate_WC.py
@@ -11,6 +11,8 @@ import numpy as np
 from estimation.model import NNWcEstModel
 
 LF_MODEL_DIR = "/home/cbs51/code/slurm_gm_workflow/estimation/models/LF"
+HF_MODEL_DIR = "/home/cbs51/code/slurm_gm_workflow/estimation/models/HF"
+BB_MODEL_DIR = "/home/cbs51/code/slurm_gm_workflow/estimation/models/BB"
 
 MODEL_PREFIX = "model_"
 SCALER_PREFIX = "scaler_"
@@ -26,7 +28,7 @@ def estimate_LF_WC_single(
 
     Params
     ------
-    nx, ny, nz, ny: float
+    nx, ny, nz, nt: float
         Input features for the model
 
     Returns
@@ -38,14 +40,68 @@ def estimate_LF_WC_single(
     # The order of the features has to the same as for training!!
     data = np.array([nx, ny, nz, nt]).reshape(1, 4)
 
-    return estimate_LF_WC(data, model_dir=model_dir, model_prefix=model_prefix,
-                          scaler_prefix=scaler_prefix)
+    return estimate(data, model_dir=model_dir, model_prefix=model_prefix,
+                    scaler_prefix=scaler_prefix)
 
 
-def estimate_LF_WC(input_data: np.ndarray, model_dir: str = LF_MODEL_DIR,
-                   model_prefix: str = MODEL_PREFIX,
-                   scaler_prefix: str = SCALER_PREFIX):
-    """Function to use for making estimations
+def estimate_HF_WC_single(
+        fd_count: int, nsub_stoch: float, nt: float, model_dir: str = HF_MODEL_DIR,
+        model_prefix: str = MODEL_PREFIX, scaler_prefix: str = SCALER_PREFIX):
+    """Convenience function to make a single estimation
+
+    If the input parameters (or even just the order) of the model
+    is ever changed, then this function has to be adjusted accordingly.
+
+    Params
+    ------
+    fd_count, nsub_stoch, nt: int, float
+        Input features for the model
+
+    Returns
+    -------
+    wc: float
+        Estimated wall clock time
+    """
+    # Make a numpy array of the input data in the right shape
+    # The order of the features has to the same as for training!!
+    data = np.array([fd_count, nsub_stoch, nt]).reshape(1, 3)
+
+    return estimate(data, model_dir=model_dir, model_prefix=model_prefix,
+                    scaler_prefix=scaler_prefix)
+
+
+def estimate_BB_WC_single(
+        fd_count: int, nt: float, model_dir: str = BB_MODEL_DIR,
+        model_prefix: str = MODEL_PREFIX, scaler_prefix: str = SCALER_PREFIX):
+    """Convenience function to make a single estimation
+
+    If the input parameters (or even just the order) of the model
+    is ever changed, then this function has to be adjusted accordingly.
+
+    Params
+    ------
+    fd_count, nt: int, float
+        Input features for the model
+        Where nt is the nt from HF
+
+
+    Returns
+    -------
+    wc: float
+        Estimated wall clock time
+    """
+    # Make a numpy array of the input data in the right shape
+    # The order of the features has to the same as for training!!
+    data = np.array([fd_count, nt]).reshape(1, 2)
+
+    return estimate(data, model_dir=model_dir, model_prefix=model_prefix,
+                    scaler_prefix=scaler_prefix)
+
+
+def estimate(input_data: np.ndarray, model_dir: str,
+             model_prefix: str = MODEL_PREFIX,
+             scaler_prefix: str = SCALER_PREFIX):
+    """Function to use for making estimations using a pre-trained model
 
     Scales the input data and then returns the estimations
     from the loaded model.
