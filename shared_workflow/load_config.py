3390e7b47e129c77cde1936c9e2c1859a4c0c44e
claudio <claudio.schill@canterbury.ac.nz>
2018-12-19 14:56:46 +1300

    Made load_config.py python 2 and 3 compatible.

--- shared_workflow/load_config.py
+++ shared_workflow/load_config.py
@@ -1,12 +1,10 @@
-
-import sys
 import os
 import json
 
-def load(directory=os.path.dirname(os.path.abspath(__file__)), cfg_name="workflow_config.json"):
-    #directory = os.path.dirname(os.path.abspath(__file__))
-    print directory
-    #config_file = os.path.join(directory, "workflow_config.json")
+# Shouldn't this be in shared or qcore?
+def load(directory=os.path.dirname(os.path.abspath(__file__)),
+         cfg_name="workflow_config.json"):
+    print(directory)
     config_file = os.path.join(directory, cfg_name)
     try:
         with open(config_file) as f:
@@ -14,8 +12,9 @@ def load(directory=os.path.dirname(os.path.abspath(__file__)), cfg_name="workflo
             return config_dict
 
     except IOError:
-        print "No %s available on %s" %(cfg_name,directory)
-        print "This is a fatal error. Please contact someone from the software team."
+        print("No %s available on %s" % (cfg_name, directory))
+        print("This is a fatal error. Please contact someone "
+              "from the software team.")
         exit(1)
 
 
