0ded08118cc544c68c6e43bbfbb75fd4736f5211
ttgenius <melodyzyz@gmail.com>
2019-01-15 16:24:31 +1300

    changed custom path back

--- scripts/cybershake/run_queue_and_auto_submit.sh
+++ scripts/cybershake/run_queue_and_auto_submit.sh
@@ -22,7 +22,7 @@ do
             mkdir /tmp/cer
             chmod 777 /tmp/cer
         fi
-        cmd="/home/melody.zhu/slurm_gm_workflow/scripts/cybershake/run_db_queue.sh $path_sim_root"
+        cmd="$gmsim/workflow/scripts/cybershake/run_db_queue.sh $path_sim_root"
         echo $cmd
         $cmd
     done
@@ -32,7 +32,7 @@ do
         mkdir /tmp/cer
         chmod 777 /tmp/cer/
     fi
-    cmd="python /home/melody.zhu/slurm_gm_workflow/scripts/cybershake/auto_submit.py $path_sim_root --config $cybershake_cfg --user `whoami`"
+    cmd="python $gmsim/workflow/scripts/cybershake/auto_submit.py $path_sim_root --config $cybershake_cfg --user `whoami`"
     echo $cmd
     $cmd
     sleep $interval
