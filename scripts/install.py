979c1b1d016b020da40167522023382bec823f69
ttgenius <melodyzyz@gmail.com>
2019-01-24 00:24:48 +0000

    all tested

--- scripts/install.py
+++ scripts/install.py
@@ -21,28 +21,7 @@ from management import create_mgmt_db
 
 # TODO: namespacing
 from shared_workflow.shared import *
-
-
-workflow_config = ldcfg.load(os.path.dirname(os.path.realpath(__file__)), "workflow_config.json")
-workflow_root = workflow_config['gm_sim_workflow_root']
-global_root = workflow_config["global_root"]
-bin_process_dir = os.path.join(global_root, 'workflow/scripts')
-emod3d_version = workflow_config["emod3d_version"]
-params_vel = workflow_config['params_vel']
-
-run_dir = workflow_config['runfolder_path']
-user = getpass.getuser()
-user_root = os.path.join(run_dir, user)  # global_root
-srf_default_dir = os.path.join(global_root, 'RupModel')
-vel_mod_dir = os.path.join(global_root, 'VelocityModels')
-recipe_dir = workflow_config['templates_dir']
-v_mod_1d_dir = os.path.join(global_root, 'VelocityModel', 'Mod-1D')
-gmsa_dir = os.path.join(global_root, 'groundMotionStationAnalysis')
-stat_dir = os.path.join(global_root, 'StationInfo')
-tools_dir = os.path.join(global_root, 'opt/maui/emod3d/3.0.4-gcc/bin')
-
-latest_ll_dir = os.path.join(global_root, 'StationInfo/grid')
-latest_ll = 'non_uniform_with_real_stations_latest'
+from shared_workflow.shared_defaults import *
 
 # default values
 # TODO: after enabling different dt for LF and HF, this might need to change
