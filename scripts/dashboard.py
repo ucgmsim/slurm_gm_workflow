556a538ef35a0e63ff1091a9f2141d211b02970a
TTgenius <melodyzyz@gmail.com>
2019-01-28 19:57:17 +1300

    run python with cron tested successfully on local, permission issue on hypo

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -1,34 +1,79 @@
+#!/usr/bin/env python3
 import os
 import subprocess
-from crontab import CronTab
+import argparse
+import getpass
 from datetime import datetime
 from qcore import utils
 
-user = 'melody.zhu'
-
-hpc = 'maui'
-
-ch_report_path = '/nesi/project/nesi00213/workflow/scripts/CH_report.sh'
-
-out_dir = '/home/yzh231/CH_usage'
-
-period = 2
-
-
+MAX_NODES = 264
+CH_REPORT_PATH = '/nesi/project/nesi00213/workflow/scripts/CH_report.sh'
 PROJECT_ID = 'nesi00213'
-SSH_CMD = "ssh {}@{}".format(user, hpc)
 TIME_FORMAT = "%Y_%m_%d-%H_%M_%S"
-TIME = datetime.now().strftime(TIME_FORMAT)
+HPCS = ['maui', 'mahuika']
+OUT_DIR = os.path.join('/home', getpass.getuser(),'CH_usage')
 
-HIS_CH_CMD = "{} bash {} {} >> {}_{}.txt".format(SSH_CMD, ch_report_path, period, 'his_ch_usage', TIME)
 
-RT_CH_CMD = "{} nn_corehour_usage {} >> {}_{}.txt".format(SSH_CMD, PROJECT_ID, 'rt_ch_usage', TIME)
+def run_dashboard_cmds(user, hpc, period, out_dir):
+    ssh_cmd = "ssh {}@{}".format(user, hpc)
 
-RT_QUOTA_CMD = "{} nn_check_quota >> {}_{}.txt".format(SSH_CMD, 'rt_quota_usage', TIME)
+    time = datetime.now().strftime(TIME_FORMAT)
 
-SQ_CMD = "{} squeue >> {}_{}.txt".format(SSH_CMD, 'squeue', TIME)
+    sub_outdir = os.path.join(out_dir,'{}_{}'.format(hpc, time))
+    utils.setup_dir(sub_outdir)
+    os.chdir(sub_outdir)
 
-# subprocess.call(HIS_CH_CMD, shell=True, encoding='utf-8')
-# subprocess.call(RT_CH_CMD, shell=True, encoding='utf-8')
-# subprocess.call(RT_QUOTA_CMD, shell=True, encoding='utf-8')
-subprocess.call(SQ_CMD, shell=True, encoding='utf-8')
+    his_ch_cmd = "{} bash {} {} >> {}_{}.txt".format(ssh_cmd, CH_REPORT_PATH, period, 'his_ch_usage', time)
+
+    rt_ch_cmd = "{} nn_corehour_usage {} >> {}_{}.txt".format(ssh_cmd, PROJECT_ID, 'rt_ch_usage', time)
+
+    rt_quota_cmd = "{} nn_check_quota >> {}_{}.txt".format(ssh_cmd, 'rt_quota_usage', time)
+
+    sq_cmd = "{} squeue >> {}_{}.txt".format(ssh_cmd, 'squeue', time)
+
+    capa_cmd = ssh_cmd + ' ' + "squeue -p nesi_research | awk '{print $10}'"
+
+    subprocess.call(his_ch_cmd, shell=True)
+    subprocess.call(rt_ch_cmd, shell=True)
+    subprocess.call(rt_quota_cmd, shell=True)
+    subprocess.call(sq_cmd, shell=True)
+    output = subprocess.check_output(capa_cmd, shell=True).decode('utf-8').strip().split('\n')   # 'NODES\n23\n32\n'---> ['NODES', '23', '32']
+
+    total_nodes = 0
+    for i in range(len(output)):
+        try:
+            total_nodes += int(output[i])
+        except ValueError:
+            continue
+
+    capcacity = (1 - total_nodes / MAX_NODES) * 100.
+
+    s = "{}: Avavilable node capacity on partition nesi_research is {:.3f}%. {} nodes in use, Max nodes {}".format(hpc, capcacity, total_nodes, MAX_NODES)
+    with open('capacity_{}.txt'.format(time), 'w') as f:
+        f.write(s)
+    print(s)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user', default='melody.zhu',
+                        help="hpc user name.eg. melody.zhu")
+    parser.add_argument('period', default=2,
+                        help="Please provide period(time interval) for CH_Report to run, default is 2")
+    parser.add_argument('-o', '--out_dir', default=OUT_DIR,
+                        help="Please provide out_dir for storing all core hour usage data")
+    parser.add_argument('--hpc', choices=HPCS,
+                        help="Please specify which hpc to log on, default both")
+
+    args = parser.parse_args()
+    utils.setup_dir(args.out_dir)
+
+    if args.hpc is not None:
+        run_dashboard_cmds(args.user, args.hpc, args.period, args.out_dir)
+    else:
+        for hpc in HPCS:
+            run_dashboard_cmds(args.user, hpc, args.period, args.out_dir)
+
+
+if __name__ == '__main__':
+    main()
