9d2e131cc950ce354eb5f41c94f5476058d3c7c8
ttgenius <melodyzyz@gmail.com>
2019-01-30 16:36:03 +1300

    qcofnig path changed to shared_defaults

--- scripts/hf_sim.py
+++ scripts/hf_sim.py
@@ -12,7 +12,7 @@ from tempfile import mkstemp
 from mpi4py import MPI
 import numpy as np
 
-from qcore.config import qconfig
+from shared_workflow import shared_defaults
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -59,7 +59,7 @@ if is_master:
     arg(
         "--sim_bin",
         help="high frequency binary (modified for binary out)",
-        default=os.path.join(qconfig["tools_dir"], "hb_high_v5.4.5_binmod"),
+        default=os.path.join(shared_defaults.tools_dir, "hb_high_v5.4.5_binmod"),
     )
     arg("--t-sec", help="high frequency output start time", type=float, default=0.0)
     # HF IN, line 1
@@ -125,7 +125,7 @@ if is_master:
         "-m",
         "--velocity-model",
         help="path to velocity model (1D)",
-        default=os.path.join(qconfig["VEL_MOD"], "Mod-1D/Cant1D_v2-midQ_leer.1d"),
+        default=os.path.join(shared_defaults.vel_mod_dir, "Mod-1D/Cant1D_v2-midQ_leer.1d"),
     )
     arg("-s", "--site-vm-dir", help="dir containing site specific velocity models (1D)")
     # HF IN, line 14
