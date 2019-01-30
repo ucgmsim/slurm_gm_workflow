9b827ed8d25ecd64555d0c1360d5fa4af7848fb9
ttgenius <melodyzyz@gmail.com>
2019-01-30 14:29:28 +1300

    tools_dir now being dynamiclly determined, to test on maui

--- shared_workflow/shared_defaults.py
+++ shared_workflow/shared_defaults.py
@@ -2,6 +2,7 @@ import os
 import getpass
 from shared_workflow import load_config as ldcfg
 import scripts
+from qcore import config
 
 workflow_config = ldcfg.load(os.path.dirname(os.path.realpath(scripts.__file__)), "workflow_config.json")
 workflow_root = workflow_config['gm_sim_workflow_root']
@@ -18,7 +19,7 @@ vel_mod_dir = os.path.join(global_root, 'VelocityModel')
 recipe_dir = workflow_config['templates_dir']
 v_mod_1d_dir = os.path.join(global_root, 'VelocityModel', 'Mod-1D')
 stat_dir = os.path.join(global_root, 'StationInfo')
-tools_dir = os.path.join(global_root, 'opt/maui/emod3d/3.0.4-gcc/bin')
+tools_dir = config.get_host_config('emod3d', '3.0.4-gcc')['tools_dir']
 
 latest_ll_dir = os.path.join(global_root, 'StationInfo/grid')
 latest_ll = 'non_uniform_with_real_stations_latest'
