ee09039a1e65cea58612fe6987c17c09612e15ab
jpa198 <james.paterson@canterbury.ac.nz>
2019-02-15 12:10:54 +1300

    Added seis file integrity check Changed filename manipulation to use sed Added messages to say why the check failed Added check for number of cpus and number of rlog files

--- scripts/test_emod3d.sh
+++ scripts/test_emod3d.sh
@@ -21,32 +21,63 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
+fileCount=`ls -1|wc -l`
+
+#Check that the number of cores and number of rlog files is the same
+if [[ ! -z ${SLURM_CPUS_ON_NODE} ]] && [[ ${SLURM_CPUS_ON_NODE} != ${fileCount} ]]
+then
+    echo "Number of cores and number of log files mismatch"
+    exit 1
+fi
+
 rlog_count=$(expr 0)
 rlog_check=0
 for rlog in *;
 do
-
-    seisFile=${rlog//-0/_seis-0}
-    seisFile="../OutBin/${seisFile/%.rlog/.e3d}"
-
-    xytsFile=${rlog//-0/_xyts-0}
-    xytsFile="../OutBin/${xytsFile/%.rlog/.e3d}"
-
     rlog_count=`expr $rlog_count + 1`
     grep "IS FINISHED" $rlog >>/dev/null
-    if [[ $? == 0 ]] && [[ -f "$seisFile" ]] && [[ -f "$xytsFile" ]];
+    if [[ $? == 0 ]];
     then
         rlog_check=0
 #            echo "rlog =1"
     else
         rlog_check=1
-#        echo "$rlog not finsihed"
+        echo "$rlog not finished"
         break
+    fi
 
-fi
+    rootFile="../OutBin/${rlog/%.rlog/.e3d}"
+
+    seisFile=`echo $rootFile | sed 's/\(.*\)-/\1_seis-/'`
+    xytsFile=`echo $rootFile | sed 's/\(.*\)-/\1_xyts-/'`
+
+    #Check that if the seis or xyts file exists for a certain integer then the other also exists
+    if [[ -f "$seisFile" ]] && [[ ! -f "$xytsFile" ]];
+    then
+        echo "Found the seis file $seisFile, but didn't find the matching xyts file $xytsFile"
+        rlog_check=1
+        break
+    fi
+    if [[ ! -f "$seisFile" ]] && [[ -f "$xytsFile" ]];
+    then
+        echo "Found the xyts file $xytsFile, but didn't find the matching seis file $seisFile"
+        rlog_check=1
+        break
+    fi
 done
 
+#Check the integrity of the seisfiles
 if [[ $rlog_check == 0 ]];
+then
+    python3 -c "from qcore import timeseries; timeseries.LFSeis('../OutBin');" 2>/dev/null
+    seisIntegrity=$?
+    if [[ $seisIntegrity != 0 ]];
+    then
+        echo "At least one file failed the integrity check"
+    fi
+fi
+
+if [[ $rlog_check == 0 ]] && [[ $seisIntegrity == 0 ]];
 then
     echo "$lf_sim_dir: EMOD3D completed"
     exit 0
