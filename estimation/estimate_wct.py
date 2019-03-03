Incorrect max time formula changed. Removed aliasing issue

estimation/estimate_wct.py
@@ -141,10 +141,9 @@ def estimate_LF_chours(
         scaler_prefix=scaler_prefix,
     )
 
-    n_cpus = data[:, -1]
-    wct = (core_hours / n_cpus)
+    wct = (core_hours / data[:, -1])
 
-    if scale_ncores and np.any(wct > (node_time_th_factor*n_cpus/PHYSICAL_NCORES_PER_NODE)):
+    if scale_ncores and np.any(wct > (node_time_th_factor * data[:, -1] / PHYSICAL_NCORES_PER_NODE)):
         # Want to scale, and at least one job exceeds the allowable time for the given number of cores
         return scale_core_hours(
             core_hours,
@@ -152,7 +151,7 @@ def estimate_LF_chours(
             node_time_th_factor,
         )
     else:
-        return core_hours, core_hours / n_cpus, n_cpus
+        return core_hours, core_hours / data[:, -1], (data[:, -1])
 
 
 def est_HF_chours_single(
@@ -294,8 +293,9 @@ def scale_core_hours(
     # All computation is in terms of nodes
     n_nodes = data[:, -1] / PHYSICAL_NCORES_PER_NODE
     estimated_nodes = np.ceil(np.sqrt(core_hours / (node_time_th_factor*PHYSICAL_NCORES_PER_NODE)))
-    if (estimated_nodes*node_time_th_factor*PHYSICAL_NCORES_PER_NODE) > MAX_JOB_WCT:
-        estimated_nodes = np.ceil((core_hours / MAX_JOB_WCT) / PHYSICAL_NCORES_PER_NODE)
+    mask = (estimated_nodes*node_time_th_factor) > MAX_JOB_WCT
+    if np.any(mask):
+        estimated_nodes[mask] = np.ceil((core_hours[mask] / MAX_JOB_WCT) / PHYSICAL_NCORES_PER_NODE)
     n_nodes = np.minimum(np.maximum(estimated_nodes, n_nodes), MAX_NODES_PER_JOB)
     n_cpus = n_nodes*PHYSICAL_NCORES_PER_NODE
     return core_hours, core_hours/n_cpus, n_cpus