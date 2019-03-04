7e3a370ea077eb4ccd18275438fc377979bf8d4b
jpa198 <james.paterson@canterbury.ac.nz>
2019-03-04 16:19:12 +1300

    Qcore is installed to each virtual environment No need to have the opt version in the python path

--- install/python3_load_functions.sh
+++ install/python3_load_functions.sh
@@ -11,9 +11,6 @@ load_python2_maui () {
         # Reset the PYTHONPATH
         export PYTHONPATH=/opt/python/2.7.15.1/lib/python2.7/site-packages
 
-        # PYTHONPATH (this can be removed once qcore is installed as a pip package)
-        export PYTHONPATH=/nesi/project/nesi00213/opt/maui/qcore:$PYTHONPATH
-
         # PYTHONPATH for workflow
         export PYTHONPATH=/nesi/project/nesi00213/workflow:$PYTHONPATH
 
@@ -31,9 +28,6 @@ load_python3_maui () {
         # Reset the PYTHONPATH
         export PYTHONPATH=/opt/python/3.6.5.1/lib/python3.6/site-packages
 
-        # PYTHONPATH (this can be removed once qcore is installed as a pip package)
-        export PYTHONPATH=$PYTHONPATH:/nesi/project/nesi00213/opt/maui/qcore
-
         # PYTHONPATH for workflow
         export PYTHONPATH=$PYTHONPATH:/nesi/project/nesi00213/workflow
 
@@ -49,9 +43,6 @@ load_python2_mahuika () {
         # Reset the PYTHONPATH
         export PYTHONPATH=''
 
-        # PYTHONPATH (this can be removed once qcore is installed as a pip package)
-        export PYTHONPATH=/nesi/project/nesi00213/opt/mahuika/qcore:$PYTHONPATH
-
         # PYTHONPATH for workflow
         export PYTHONPATH=/nesi/project/nesi00213/workflow:$PYTHONPATH
 
@@ -66,9 +57,6 @@ load_python3_mahuika () {
 
         # Reset the PYTHONPATH
         export PYTHONPATH=''
-
-        # PYTHONPATH (this can be removed once qcore is installed as a pip package)
-        export PYTHONPATH=/nesi/project/nesi00213/opt/mahuika/qcore:$PYTHONPATH
 
         # PYTHONPATH for workflow
         export PYTHONPATH=/nesi/project/nesi00213/workflow:$PYTHONPATH
