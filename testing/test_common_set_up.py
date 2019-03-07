55e6ae8885c19bb8db4bca3f8ed93d6b7b088d84
ttgenius <melodyzyz@gmail.com>
2019-03-07 14:32:52 +1300

    added common funcs

--- testing/test_common_set_up.py
+++ testing/test_common_set_up.py
@@ -1,4 +1,6 @@
+import os
 import pytest
+import pickle
 
 from qcore import utils
 
@@ -10,6 +12,26 @@ REALISATIONS = [
         "http://ec2-54-206-55-199.ap-southeast-2.compute.amazonaws.com/static/public/testing/slurm_gm_workflow/PangopangoF29_HYP01-10_S1244.zip",
     )
 ]
+
+
+def get_input_params(root_path, func_name, params):
+    input_params = []
+    for param in params:
+        print(os.path.join(root_path, INPUT, func_name + "_{}.P".format(param)))
+        with open(
+                os.path.join(root_path, INPUT, func_name + "_{}.P".format(param)), "rb"
+        ) as load_file:
+            input_param = pickle.load(load_file)
+            input_params.append(input_param)
+    return input_params
+
+
+def get_bench_output(root_path, func_name):
+    with open(
+            os.path.join(root_path, OUTPUT, func_name + "_ret_val.P"), "rb"
+    ) as load_file:
+        bench_output = pickle.load(load_file)
+    return bench_output
 
 
 @pytest.yield_fixture(scope="session", autouse=True)
--- testing/test_shared_workflow/test_shared.py
+++ testing/test_shared_workflow/test_shared.py
@@ -1,57 +1,30 @@
 import os
 import pickle
+import inspect
 
 from shared_workflow import shared
-from testing.test_common_set_up import INPUT, OUTPUT, set_up
+from testing.test_common_set_up import INPUT, OUTPUT, set_up, get_input_params, get_bench_output
 
 
 def test_get_stations(set_up):
     func_name = 'get_stations'
+    params = inspect.getfullargspec(shared.get_stations).args
     for content in set_up:
         for root_path in content:
-            with open(
-                    os.path.join(root_path, INPUT, func_name + "_source_file.P"), "rb"
-            ) as load_file:
-                source_file = pickle.load(load_file)
-            with open(
-                    os.path.join(root_path, INPUT, func_name + "_locations.P"), "rb"
-            ) as load_file:
-                locations = pickle.load(load_file)
-
-            test_output = shared.get_stations(source_file, locations)
-
-            with open(
-                    os.path.join(root_path, OUTPUT, func_name + "_stations.P"), "rb"
-            ) as load_file:
-                stations = pickle.load(load_file)
-
-            with open(
-                    os.path.join(root_path, OUTPUT, func_name + "_station_lats.P"), "rb"
-            ) as load_file:
-                station_lats = pickle.load(load_file)
-
-            with open(
-                    os.path.join(root_path, OUTPUT, func_name + "_station_lons.P"), "rb"
-            ) as load_file:
-                station_lons = pickle.load(load_file)
-
-            assert test_output == (stations, station_lats, station_lons)
+            input_params = get_input_params(root_path, func_name, params)
+            test_output = shared.get_stations(*input_params)
+            bench_output = get_bench_output(root_path, func_name)
+            assert test_output == bench_output
 
 
 def test_user_select(set_up):
     func_name = 'user_select'
+    params = inspect.getfullargspec(shared.get_stations).args
     for content in set_up:
         for root_path in content:
-            with open(
-                    os.path.join(root_path, INPUT, func_name + "_options.P"), "rb"
-            ) as load_file:
-                options = pickle.load(load_file)
-
-            test_output = shared.user_select(options)
-            with open(
-                    os.path.join(root_path, OUTPUT, func_name + "_selected_number.P"), "rb"
-            ) as load_file:
-                bench_output = pickle.load(load_file)
+            input_params = get_input_params(root_path, func_name, params)
+            test_output = shared.user_select(*input_params)
+            bench_output = get_bench_output(root_path, func_name)
             assert test_output == bench_output
 
 
