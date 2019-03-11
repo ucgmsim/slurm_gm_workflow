0985d9da29c8865c8daf836d517e2e04171ae3e4
ttgenius <melodyzyz@gmail.com>
2019-03-12 11:52:24 +1300

    seperation of assert

--- testing/test_scripts/test_managemnet/test_mgmt_db.py
+++ testing/test_scripts/test_managemnet/test_mgmt_db.py
@@ -10,9 +10,8 @@ TEST_RUN_NAME = 'PangopangoF29_HYP01-10_S1244'
 TEST_PROC = (2, 'merge_ts')
 TEST_STATUS = (4, 'completed')
 FORCE_STATUS = (5, 'failed')
-TEST_TABLE = 'state'
 INIT_DB_ROWS = 10
-EXPECTED_ERROS = ['test_err', 'Process failed retrying', 'Reseting retries', 'another error']
+EXPECTED_ERROS = ['test_err', '', 'Process failed retrying', 'Reseting retries', 'another error']
 
 
 def setup_module(module):
@@ -27,12 +26,11 @@ def mgmt_db():
 def get_rows(mgmt_db, table, col_name, col_value, selected_col='*'):
     sql = "SELECT {} from {} where {} = ?".format(selected_col, table, col_name)
     rows = mgmt_db.execute(sql, (col_value,)).fetchall()
-    print(rows)
     return rows
 
 
 def test_create_mgmt_db(mgmt_db):
-    assert len(get_rows(mgmt_db, TEST_TABLE, 'run_name', TEST_RUN_NAME)) == INIT_DB_ROWS
+    assert len(get_rows(mgmt_db, 'state', 'run_name', TEST_RUN_NAME)) == INIT_DB_ROWS
 
 
 def test_get_procs(mgmt_db):
@@ -43,12 +41,12 @@ def test_get_procs(mgmt_db):
 
 def test_insert_task(mgmt_db):
     create_mgmt_db.insert_task(mgmt_db, TEST_RUN_NAME, TEST_PROC[0])
-    assert len(get_rows(mgmt_db, TEST_TABLE, 'proc_type', TEST_PROC[0])) == 1
+    assert len(get_rows(mgmt_db, 'state', 'proc_type', TEST_PROC[0])) == 1
 
 
 def test_update_db(mgmt_db):
     update_mgmt_db.update_db(mgmt_db, TEST_PROC[1], TEST_STATUS[1], run_name=TEST_RUN_NAME)
-    value = get_rows(mgmt_db, TEST_TABLE, 'proc_type', TEST_PROC[0], selected_col='status')[0][0]
+    value = get_rows(mgmt_db, 'state', 'proc_type', TEST_PROC[0], selected_col='status')[0][0]
     assert value == TEST_STATUS[0]
 
 
@@ -66,14 +64,19 @@ def test_update_task_time(mgmt_db):
 
 
 def test_force_update_db(mgmt_db):
-    update_mgmt_db.force_update_db(mgmt_db, TEST_PROC[1], FORCE_STATUS[1], run_name=TEST_RUN_NAME, retry=True, reset_retries=True, error='another error')
+    update_mgmt_db.force_update_db(mgmt_db, TEST_PROC[1], FORCE_STATUS[1], run_name=TEST_RUN_NAME)
     value = get_rows(mgmt_db, 'state', 'proc_type', TEST_PROC[0], selected_col='status')[0][0]
+    assert value == FORCE_STATUS[0]
+
+
+def test_force_update_db_error(mgmt_db):
+    update_mgmt_db.force_update_db(mgmt_db, TEST_PROC[1], FORCE_STATUS[1], run_name=TEST_RUN_NAME, retry=True,
+                                   reset_retries=True, error='another error')
     rows = get_rows(mgmt_db, 'error', 'task_id', TEST_PROC[0], selected_col='error')
     errors = []
     for row in rows:
         for err in row:
             errors.append(err)
-    assert value == FORCE_STATUS[0]
     assert errors == EXPECTED_ERROS
 
 
