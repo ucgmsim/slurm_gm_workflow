66eb3f9f1ade183b73974e900e2d14946196d17e
ttgenius <melodyzyz@gmail.com>
2019-03-12 16:43:59 +1300

    added test for install.py

--- testing/test_shared_workflow/test_shared.py
+++ testing/test_shared_workflow/test_shared.py
@@ -8,21 +8,19 @@ from testing.test_common_set_up import INPUT, OUTPUT, set_up, get_input_params, 
 def test_get_stations(set_up):
     func_name = 'get_stations'
     params = inspect.getfullargspec(shared.get_stations).args
-    for content in set_up:
-        for root_path in content:
-            input_params = get_input_params(root_path, func_name, params)
-            test_output = shared.get_stations(*input_params)
-            bench_output = get_bench_output(root_path, func_name)
-            assert test_output == bench_output
+    for root_path, _ in set_up:
+        input_params = get_input_params(root_path, func_name, params)
+        test_output = shared.get_stations(*input_params)
+        bench_output = get_bench_output(root_path, func_name)
+        assert test_output == bench_output
 
 
 def test_user_select(set_up):
     func_name = 'user_select'
     params = inspect.getfullargspec(shared.get_stations).args
-    for content in set_up:
-        for root_path in content:
-            input_params = get_input_params(root_path, func_name, params)
-            test_output = shared.user_select(*input_params)
-            bench_output = get_bench_output(root_path, func_name)
-            assert test_output == bench_output
+    for root_path, _ in set_up:
+        input_params = get_input_params(root_path, func_name, params)
+        test_output = shared.user_select(*input_params)
+        bench_output = get_bench_output(root_path, func_name)
+        assert test_output == bench_output
 
