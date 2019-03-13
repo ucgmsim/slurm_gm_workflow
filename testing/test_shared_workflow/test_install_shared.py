fac820cefbf4a28476c2bd1dbc4658b1af51a7ca
jpa198 <james.paterson@canterbury.ac.nz>
2019-03-13 15:23:08 +1300

    Resolve issues related to updated set_up function

--- testing/test_shared_workflow/test_install_shared.py
+++ testing/test_shared_workflow/test_install_shared.py
@@ -16,20 +16,18 @@ from testing.test_common_set_up import (
 def test_install_simulation(set_up):
     func_name = "install_simulation"
     params = inspect.getfullargspec(install_shared.install_simulation).args
-    for content in set_up:
-        for root_path in content:
-            input_params = get_input_params(root_path, func_name, params)
-            test_output = install_shared.install_simulation(*input_params)
-            bench_output = get_bench_output(root_path, func_name)
-            assert test_output == bench_output
+    for root_path, realisation in set_up:
+        input_params = get_input_params(root_path, func_name, params)
+        test_output = install_shared.install_simulation(*input_params)
+        bench_output = get_bench_output(root_path, func_name)
+        assert test_output == bench_output
 
 
 def test_install_bb(set_up):
     func_name = "install_bb"
     params = inspect.getfullargspec(install_shared.install_bb).args
-    for content in set_up:
-        for root_path in content:
-            input_params = get_input_params(root_path, func_name, params)
-            test_output = install_shared.install_bb(*input_params)
-            bench_output = get_bench_output(root_path, func_name)
-            assert test_output == bench_output
+    for root_path, realisation in set_up:
+        input_params = get_input_params(root_path, func_name, params)
+        install_shared.install_bb(*input_params)
+        bench_output = get_bench_output(root_path, func_name)
+        assert input_params[1] == bench_output
