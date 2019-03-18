7d9f30cc16a16cdd68f76cd596a016c7a95d2224
jpa198 <james.paterson@canterbury.ac.nz>
2019-03-19 12:04:23 +1300

    Added an observed/simulated flag Changed most logic in check_im_calc_completion so that it only gets one level of indentation Made number of stations a required parameter

--- scripts/im_calc_checkpoint.py
+++ scripts/im_calc_checkpoint.py
@@ -29,7 +29,9 @@ def print_verbose(message, verbose):
         print(message)
 
 
-def check_im_calc_completion(output_dir, station_count, verbose=False, event_name=None):
+def check_im_calc_completion(
+    output_dir, station_count, verbose=False, observed=False, event_name=None
+):
     """
     Check for any csv files in the given folder, or stations folder inside.
     Check for any imcalc.info files.
@@ -40,37 +42,43 @@ def check_im_calc_completion(output_dir, station_count, verbose=False, event_nam
     :param verbose: Print extra information or not
     :return: True if all conditions for job completion are true, false otherwise
     """
-    if os.path.isdir(output_dir):  # if output dir exists
+    if not os.path.isdir(output_dir):
+        print_verbose("Output directory does not exist", verbose)
+        return False
 
-        sum_csv = glob.glob1(output_dir, CSV_PATTERN)
-        if len(sum_csv) > 0 and station_count > 0:
-            if event_name:
-                sum_csv = [f for f in sum_csv if event_name in f]
-                if not sum_csv:
-                    print_verbose("No csv file found", verbose)
-                    return False
+    sum_csv = glob.glob1(output_dir, CSV_PATTERN)
 
-            csv_file_name = sum_csv[0]
+    if observed:
+        sum_csv = [f for f in sum_csv if event_name in f]
 
-            with open(os.path.join(output_dir, csv_file_name)) as csv_file:
-                if station_count != (len(csv_file.readlines()) - 1):
-                    print_verbose("CSV file does not have enough lines in it", verbose)
-                    return False
+    if len(sum_csv) < 1:
+        print_verbose("No csv file found", verbose)
+        return False
 
-        station_dir = os.path.join(output_dir, "stations")
-        if os.path.isdir(station_dir) and not event_name:
-            station_files = glob.glob1(station_dir, CSV_PATTERN)
-            if 0 < station_count != len(station_files):
-                print_verbose("Not enough files in the station directory", verbose)
-                return False
-            sum_csv = sum_csv + station_files
-        meta = glob.glob1(output_dir, META_PATTERN)
-        # if sum_csv and meta are not empty lists('.csv' and '_imcalc.info' files present)
-        # then we think im calc on the corresponding dir is completed and hence remove
-        print_verbose("Checking csv and meta files exist", verbose)
-        return sum_csv != [] and meta != []
-    print_verbose("Output directory does not exist", verbose)
-    return False
+    if len(sum_csv) > 1:
+        print_verbose("Too many possible csv files found, exiting", verbose)
+
+    csv_file_name = sum_csv[0]
+
+    with open(os.path.join(output_dir, csv_file_name)) as csv_file:
+        if station_count != (len(csv_file.readlines()) - 1):
+            print_verbose("CSV file does not have enough lines in it", verbose)
+            return False
+
+    station_dir = os.path.join(output_dir, "stations")
+    if os.path.isdir(station_dir) and not observed:
+        # If observed then all station files for all events will be present
+        station_files = glob.glob1(station_dir, CSV_PATTERN)
+        if station_count != len(station_files):
+            print_verbose("Not enough files in the station directory", verbose)
+            return False
+        sum_csv = sum_csv + station_files
+
+    meta = glob.glob1(output_dir, META_PATTERN)
+    # if sum_csv and meta are not empty lists('.csv' and '_imcalc.info' files present)
+    # then we think im calc on the corresponding dir is completed and hence remove
+    print_verbose("Checking csv and meta files exist", verbose)
+    return sum_csv != [] and meta != []
 
 
 if __name__ == "__main__":
@@ -83,8 +91,6 @@ if __name__ == "__main__":
         "station_count",
         type=int,
         help="The number of stations in the realisation",
-        nargs="?",
-        default=-1,
     )
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="flag to echo messages"
@@ -95,11 +101,27 @@ if __name__ == "__main__":
         help="Name of the event being checked. If given it is assumed that the event was observed, rather than simulated",
         default=None,
     )
+    event_type_group = parser.add_mutually_exclusive_group(required=True)
+    event_type_group.add_argument(
+        "-o",
+        "--observed",
+        action="store_true",
+        help="The calculations being checked are from an observed event",
+    )
+    event_type_group.add_argument(
+        "-s",
+        "--simulated",
+        action="store_true",
+        help="The calculations being checked are from a simulated event",
+    )
 
     args = parser.parse_args()
 
+    if args.observed and args.event_name is None:
+        parser.error("If the event was observed the name of the even must be specified")
+
     res = check_im_calc_completion(
-        args.run_dir, args.station_count, args.verbose, args.event_name
+        args.run_dir, args.station_count, args.verbose, args.observed, args.event_name
     )
     if res:
         print_verbose(
