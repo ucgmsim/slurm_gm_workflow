e8f063599467354243530ee39c4055bcf5a8c10e
claudio <claudio.schill@canterbury.ac.nz>
2019-04-09 14:41:19 +1200

    Fixing mgmt db unittests

--- testing/test_scripts/test_managemnet/test_mgmt_db.py
+++ testing/test_scripts/test_managemnet/test_mgmt_db.py
@@ -1,10 +1,12 @@
+import os
 import shutil
 import pytest
 
 from scripts.management import create_mgmt_db, update_mgmt_db
+from scripts.management.MgmtDB import SlurmTask
 from qcore import utils
 
-TEST_DB_DIR = "output"
+TEST_DB_FILE = "./output/slurm_mgmt.db"
 TEST_SRF_FILE = "/nesi/nobackup/nesi00213/RunFolder/PangopangoF29/Data/Sources/PangopangoF29/Srf/PangopangoF29_HYP01-10_S1244.srf"
 TEST_RUN_NAME = "PangopangoF29_HYP01-10_S1244"
 TEST_PROC = (2, "merge_ts")
@@ -21,12 +23,12 @@ EXPECTED_ERROS = [
 
 
 def setup_module(module):
-    utils.setup_dir(TEST_DB_DIR)
+    utils.setup_dir(os.path.dirname(TEST_DB_FILE))
 
 
 @pytest.fixture(scope="module")
 def mgmt_db():
-    yield create_mgmt_db.create_mgmt_db([], TEST_DB_DIR, TEST_SRF_FILE)
+    yield create_mgmt_db.create_mgmt_db([], TEST_DB_FILE, TEST_SRF_FILE)
 
 
 def get_rows(mgmt_db, table, col_name, col_value, selected_col="*"):
@@ -39,30 +41,21 @@ def test_create_mgmt_db(mgmt_db):
     assert len(get_rows(mgmt_db, "state", "run_name", TEST_RUN_NAME)) == INIT_DB_ROWS
 
 
-def test_get_procs(mgmt_db):
-    proc_types = create_mgmt_db.get_procs(mgmt_db)
-    assert len(proc_types) == INIT_DB_ROWS
-    assert proc_types[5][-1] == "IM_calculation"
-
-
 def test_insert_task(mgmt_db):
-    create_mgmt_db.insert_task(mgmt_db, TEST_RUN_NAME, TEST_PROC[0])
-    assert len(get_rows(mgmt_db, "state", "proc_type", TEST_PROC[0])) == 1
+    mgmt_db.insert(TEST_RUN_NAME, TEST_PROC[0])
+    assert len(get_rows(mgmt_db.db_file, "state", "proc_type", TEST_PROC[0])) == 1
 
-
-def test_update_db(mgmt_db):
-    update_mgmt_db.update_db(
-        mgmt_db, TEST_PROC[1], TEST_STATUS[1], run_name=TEST_RUN_NAME
-    )
+def test_update_live_db(mgmt_db):
+    mgmt_db.update_entries_live(SlurmTask(TEST_RUN_NAME, TEST_PROC[0], TEST_STATUS[0]))
     value = get_rows(
-        mgmt_db, "state", "proc_type", TEST_PROC[0], selected_col="status"
+        mgmt_db.db_file, "state", "proc_type", TEST_PROC[0], selected_col="status"
     )[0][0]
     assert value == TEST_STATUS[0]
 
 
 def test_update_db_error(mgmt_db):
     update_mgmt_db.update_error(
-        mgmt_db, TEST_PROC[1], run_name=TEST_RUN_NAME, error="test_err"
+        mgmt_db.db_file, TEST_PROC[1], run_name=TEST_RUN_NAME, error="test_err"
     )
     value = get_rows(mgmt_db, "error", "task_id", TEST_PROC[0], selected_col="error")[
         0
@@ -72,23 +65,23 @@ def test_update_db_error(mgmt_db):
 
 def test_update_task_time(mgmt_db):
     update_mgmt_db.update_task_time(
-        mgmt_db, TEST_RUN_NAME, TEST_PROC[1], TEST_STATUS[1]
+        mgmt_db.db_file, TEST_RUN_NAME, TEST_PROC[1], TEST_STATUS[1]
     )
     test_time = get_rows(
-        mgmt_db, "task_time_log", "state_id", TEST_PROC[0], selected_col="time"
+        mgmt_db.db_file, "task_time_log", "state_id", TEST_PROC[0], selected_col="time"
     )[0][0]
     bench_time = get_rows(
-        mgmt_db, "state", "proc_type", TEST_PROC[0], selected_col="last_modified"
+        mgmt_db.db_file, "state", "proc_type", TEST_PROC[0], selected_col="last_modified"
     )[0][0]
     assert test_time == bench_time
 
 
 def test_force_update_db(mgmt_db):
     update_mgmt_db.force_update_db(
-        mgmt_db, TEST_PROC[1], FORCE_STATUS[1], run_name=TEST_RUN_NAME
+        mgmt_db.db_file, TEST_PROC[1], FORCE_STATUS[1], run_name=TEST_RUN_NAME
     )
     value = get_rows(
-        mgmt_db, "state", "proc_type", TEST_PROC[0], selected_col="status"
+        mgmt_db.db_file, "state", "proc_type", TEST_PROC[0], selected_col="status"
     )[0][0]
     assert value == FORCE_STATUS[0]
 
@@ -103,7 +96,7 @@ def test_force_update_db_error(mgmt_db):
         reset_retries=True,
         error="another error",
     )
-    rows = get_rows(mgmt_db, "error", "task_id", TEST_PROC[0], selected_col="error")
+    rows = get_rows(mgmt_db.db_file, "error", "task_id", TEST_PROC[0], selected_col="error")
     errors = []
     for row in rows:
         for err in row:
@@ -112,4 +105,4 @@ def test_force_update_db_error(mgmt_db):
 
 
 def teardown_module(module):
-    shutil.rmtree(TEST_DB_DIR)
+    shutil.rmtree(os.path.dirname(TEST_DB_FILE))
