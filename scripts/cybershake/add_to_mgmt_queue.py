ab2f3db6a28364747c4d8e0ec02bce433aaae792
claudio <claudio.schill@canterbury.ac.nz>
2019-04-11 16:23:50 +1200

    Tidy up.

--- scripts/cybershake/add_to_mgmt_queue.py
+++ scripts/cybershake/add_to_mgmt_queue.py
@@ -11,9 +11,17 @@ if __name__ == "__main__":
     parser.add_argument("queue_folder", type=str, help="Mgmt db queue folder")
     parser.add_argument("run_name", type=str, help="The realisation/run name")
     parser.add_argument(
-        "proc_type", type=str, help="The string value of the process type."
+        "proc_type",
+        type=str,
+        help="The string value of the process type.",
+        choices=list(const.ProcessType.iterate_str_values()),
     )
-    parser.add_argument("status", type=str, help="The string value of the status")
+    parser.add_argument(
+        "status",
+        type=str,
+        help="The string value of the status",
+        choices=list(const.Status.iterate_str_values()),
+    )
     parser.add_argument(
         "--error",
         type=str,
