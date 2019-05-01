cb00abb66e6361a9843444354e562b66ed29f3cc
jpa198 <james.paterson@canterbury.ac.nz>
2019-05-01 15:08:50 +1200

    Initial attempt to remove cybershake config Added constant for root_defaults file

--- scripts/cybershake/install_cybershake_fault.py
+++ scripts/cybershake/install_cybershake_fault.py
@@ -8,14 +8,15 @@ import os
 import sys
 import glob
 import argparse
+import yaml
 
 import shared_workflow.load_config as ldcfg
 import qcore.simulation_structure as sim_struct
 from qcore import utils, validate_vm, simulation_structure
-from qcore.constants import FaultParams
+from qcore.constants import FaultParams, ROOT_DEFAULTS_FILE_NAME
 from scripts.management import create_mgmt_db
 from shared_workflow.install_shared import install_simulation, generate_fd_files, dump_all_yamls
-
+from shared_workflow.shared_defaults import recipe_dir
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,10 +27,7 @@ def main():
         help="the path to the root of a specific version cybershake.",
     )
     parser.add_argument(
-        "config",
-        type=str,
-        default=None,
-        help="a path to a config file that constains all the required values.",
+        "version", type=str, default="16.1", help="Please specify GMSim version"
     )
     parser.add_argument(
         "vm", type=str, default=None, help="the name of the Velocity Model."
@@ -37,38 +35,23 @@ def main():
     parser.add_argument(
         "--n_rel", type=int, default=None, help="the number of realisations expected"
     )
-    parser.add_argument(
-        "--version", type=str, default="16.1", help="Please specify GMSim version"
-    )
 
     args = parser.parse_args()
-    try:
-        # try to read the config file
-        cybershake_cfg = ldcfg.load(
-            os.path.dirname(args.config), os.path.basename(args.config)
-        )
-    except Exception as e:
-        print(
-            "While loading the configuration file the following error occurred: {}".format(
-                e
-            )
-        )
-        sys.exit()
 
-    ldcfg.check_cfg_params_path(cybershake_cfg, "dt", "hf_dt")
+    config_dict = utils.load_yaml(os.path.join(recipe_dir, "gmsim", args.version, ROOT_DEFAULTS_FILE_NAME))
 
     # Load variables from cybershake config
     root_folder = os.path.abspath(args.path_cybershake)
-    sim_root_dir = os.path.join(root_folder, "Runs/")
-    srf_root_dir = os.path.join(root_folder, "Data/Sources/")
-    vm_root_dir = os.path.join(root_folder, "Data/VMs/")
+    sim_root_dir = simulation_structure.get_runs_dir(root_folder)
+    srf_root_dir = simulation_structure.get_sources_dir(root_folder)
+    vm_root_dir = simulation_structure.get_VM_dir(root_folder)
 
-    v1d_full_path = cybershake_cfg["v_1d_mod"]
+    v1d_full_path = config_dict["v_1d_mod"]
 
-    site_v1d_dir = cybershake_cfg.get("site_v1d_dir")
-    hf_stat_vs_ref = cybershake_cfg.get("hf_stat_vs_ref")
+    site_v1d_dir = config_dict.get("site_v1d_dir")
+    hf_stat_vs_ref = config_dict.get("hf_stat_vs_ref")
 
-    stat_file_path = cybershake_cfg['stat_file_path']
+    stat_file_path = config_dict['stat_file_path']
     vs30_file_path = stat_file_path.replace('.ll', '.vs30')
     vs30ref_file_path = stat_file_path.replace('.ll', '.vs30ref')
 
@@ -85,7 +68,7 @@ def main():
     vel_mod_dir = os.path.join(vm_root_dir, fault_name)
     valid_vm, message = validate_vm.validate_vm(vel_mod_dir)
     if not valid_vm:
-        message = "Error: VM {} failed {}".format(fault_name, message)
+        message = "Error: VM {} failed {}\n".format(fault_name, message)
         print(message)
         with open(error_log, "a") as error_fp:
             error_fp.write(message)
@@ -93,7 +76,7 @@ def main():
 
     # Load the variables from params_vel.py
     params_vel_path = os.path.join(vel_mod_dir, params_vel)
-    params_vel_dict=utils.load_yaml(params_vel_path)
+    params_vel_dict = utils.load_yaml(params_vel_path)
     yes_model_params = (
         False
     )  # statgrid should normally be already generated with Velocity Model
@@ -107,7 +90,7 @@ def main():
     if args.n_rel is not None and len(list_srf) != args.n_rel:
         message = (
             "Error: fault {} failed. Number of realisations do "
-            "not match number of SRF files".format(fault_name)
+            "not match number of SRF files\n".format(fault_name)
         )
         print(message)
         with open(error_log, "a") as error_fp:
@@ -120,11 +103,11 @@ def main():
     for srf in list_srf:
         # try to match find the stoch with same basename
         srf_name = os.path.splitext(os.path.basename(srf))[0]
-        stoch_file_path = os.path.join(stoch_dir, srf_name + ".stoch")
-        sim_params_file = os.path.join(sim_params_dir, srf_name + ".yaml")
+        stoch_file_path = os.path.join(stoch_dir, "{}.stoch".format(srf_name))
+        sim_params_file = os.path.join(sim_params_dir, "{}.yaml".format(srf_name))
 
         if not os.path.isfile(stoch_file_path):
-            message = "Error: Corresponding Stoch file is not found:\n{}".format(
+            message = "Error: Corresponding Stoch file is not found:\n{}\n".format(
                 stoch_file_path
             )
             print(message)
@@ -180,7 +163,7 @@ def main():
             fault_params_dict[FaultParams.stat_coords.value] = fd_statcords
             fault_params_dict[FaultParams.FD_STATLIST.value] = fd_statlist
 
-            #     root_params_dict['hf_stat_vs_ref'] = cybershake_cfg['hf_stat_vs_ref']
+            #     root_params_dict['hf_stat_vs_ref'] = config_dict['hf_stat_vs_ref']
             dump_all_yamls(
                 sim_dir,
                 root_params_dict,
