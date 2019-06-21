f647325fb862b8e81fbe365f592a0dbae10e68ba
jpa198 <james.paterson@canterbury.ac.nz>
2019-06-21 16:22:11 +1200

    Fix type mismatch

--- shared_workflow/install_shared.py
+++ shared_workflow/install_shared.py
@@ -158,7 +158,7 @@ def install_simulation(
     if stat_file_path is not None:
         sim_params_dict[SimParams.stat_file.value] = stat_file_path
 
-    if sim_duration/root_params_dict['dt'] - int(sim_duration/root_params_dict['dt']) > 0.001:
+    if float(sim_duration)/root_params_dict['dt'] - int(float(sim_duration)/root_params_dict['dt']) > 0.001:
         logger.critical(
             "Simulation dt does not match sim duration. This will result in errors during BB. Simulation duration must "
             "be a multiple of dt. Ignoring fault. Simulation_duration: {}. dt: {}.".format(
