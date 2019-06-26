553921aa2c8c593e6b311663fa80020d6a703353
ttgenius <melodyzyz@gmail.com>
2019-06-26 15:27:14 +1200

    deleted unused files

--- install_workflow/helper_functions/activate_maui_python3_virtenv.sh
+++ install_workflow/helper_functions/activate_maui_python3_virtenv.sh
@@ -12,7 +12,6 @@ module del cray-python/2.7.15.1
 
 # Reset the PYTHONPATH
 export PYTHONPATH=/opt/python/3.6.5.1/lib/python3.6/site-packages
-export PATH=$PATH:/nesi/project/nesi00213/opt/maui/ffmpeg_build/bin:/nesi/project/nesi00213/opt/maui/gmt/5.4.4/bin
 
 # Load the virtual environment
 source ${virtenv_path}/bin/activate
