9c22f757758988fa5572b17e984cf6b1e387b1cf
ttgenius <melodyzyz@gmail.com>
2019-09-19 17:36:46 +1200

    modified according to most of the comments

--- scripts/sbatch.py
+++ scripts/sbatch.py
@@ -10,13 +10,12 @@ from enum import Enum
 
 MB_PER_GB = 1024.0
 
-DEFAULT_NTASKS = 1
-DEFAULT_CPUS_PER_TASK = 1
+DEFAULTS = {'ntasks': 1, 'nodes': 1, 'cpus-per-task': 1, 'ntasks-per-core': 1, 'cpu_weights': 1, "mem_weights": 0}
 
 MAHUIKA_SLURM_CONF = (
     "/scale_wlg_persistent/filesets/home/slurm/mahuika/etc/opt/slurm/slurm.conf"
 )
-MAUI_SLUM_CONF = "/etc/opt/slurm.conf"
+MAUI_SLUM_CONF = "/scale_wlg_persistent/filesets/home/slurm/maui/etc/opt/slurm/slurm.conf.factory"
 
 SBATCH_BIN = "/opt/slurm/18.08.7/bin/sbatch"
 
@@ -32,6 +31,8 @@ class SlurmHeader(Enum):
     account = "account"
     partition = "partition"
     ntasks = "ntasks"
+    ntasks_per_core = "ntasks-per-core"
+    ncpus = "ncpus"
     cpus_per_task = "cpus-per-task"
     mem_per_cpu = "mem-per-cpu"
 
@@ -70,9 +71,12 @@ def get_default_mem_per_cpu(slurm_conf):
     for line in lines[::-1]:
         if line.startswith("PartitionName=DEFAULT"):
             # 512 /1024
-            default_mem_per_cpu = (
-                float(line.strip().split("DefMemPerCPU=")[-1].split()[0]) / MB_PER_GB
-            )
+            try:
+                default_mem_per_cpu = (
+                    float(line.strip().split("DefMemPerCPU=")[-1].split()[0]) / MB_PER_GB
+                )
+            except (IndexError, ValueError):  # maui slum.conf may not have DefMemPerCPU attribute
+                default_mem_per_cpu = 0
             return default_mem_per_cpu
 
 
@@ -93,20 +97,24 @@ def read_slurm_conf(slurm_conf, partition_name):
                 # TRESBillingWeights not found, use default cpu_weights and mem_weights
                 for l in lines[::-1]:
                     if l.startswith("PartitionName=DEFAULT"):
-                        return get_billing_weights(l)
+                        try:
+                            return get_billing_weights(l)
+                        except (IndexError, ValueError):
+                            return DEFAULTS['cpu_weights'], DEFAULTS["mem_weights"]
 
 
 def calculate_requested_chours(
-    cpu_billing_weights,
-    mem_billing_weights,
-    mem_per_cpu,
-    ntasks=DEFAULT_NTASKS,
-    cpus_per_task=DEFAULT_CPUS_PER_TASK,
+        cpu_billing_weights,
+        mem_billing_weights,
+        mem_per_cpu,
+        ntasks,
+        ntasks_per_core,
+        cpus_per_task,
 ):
     """Calculate requested core hours for a job on mahuika"""
     # https://slurm.schedmd.com/tres.html
     # see above link for calculating formula
-    total_cpus = ntasks * cpus_per_task
+    total_cpus = ntasks * ntasks_per_core * cpus_per_task
     total_mem = total_cpus * mem_per_cpu
 
     requested_hours = (total_cpus * cpu_billing_weights) + (
@@ -144,73 +152,52 @@ def get_available_chours(json_file, account, username):
             )
 
 
-def compare_hours_and_sbatch(requested_hours, available_hours, hpc, sl_to_sbatch):
-    """
-       Compare requested hour with available hours
-       and either submit the job or reject
-    """
-    if requested_hours <= available_hours:
-        cmd = "{} -M {} {}".format(SBATCH_BIN, hpc, sl_to_sbatch)
-        subprocess.call(cmd, shell=True)
-    else:
-        sys.exit("Not enought core hours left, please contact Jonney")
-
-
 def process_slurm_header(sl_file):
     """Read a slurm file and returns the wanted header values as a dictionary"""
     with open(sl_file) as f:
         lines = f.readlines()
     # SBATCH --account=nesi00213
     header_dict = {h.value: None for h in SlurmHeader}
-    for header in header_dict.keys():
-        for line in lines:
-            if "SBATCH" in line and header in line:
-                value = line.strip().split("--{}=".format(header))[-1]
-                header_dict[header] = value
-                break
+    for line in lines:
+        if line.startswith("#") and "SBATCH" in line.upper():
+            for header in header_dict.keys():
+                if "{}=".format(header) in line:
+                    value = line.strip().split("--{}=".format(header))[-1]
+                    header_dict[header] = value
+                    break
     return header_dict
 
 
 def process_mem_per_cpu(mem_per_cpu):
     """Convert a mem_per_cpu string to float"""
     if isinstance(mem_per_cpu, str):
-        if mem_per_cpu[-1].upper() == "G":  # 0.5G to 0.5
-            mem_per_cpu = float(mem_per_cpu[:-1])  # 521MB to 0.5
-        elif mem_per_cpu[-2:] == "MB":
-            mem_per_cpu = float(mem_per_cpu[:-2]) / MB_PER_GB
-    else:
-        sys.exit("undefined mem per cpu, please add unit G/MB if not included")
+        if mem_per_cpu[-1].upper() == "G" or mem_per_cpu[-2:].upper() == "GB":
+            # 0.5G to 0.5
+            mem_per_cpu = float(mem_per_cpu[:-1])
+    else:  # no unit or unit=MB, All default to MB
+        mem_per_cpu = float(mem_per_cpu[:-2]) / MB_PER_GB
     return mem_per_cpu
 
 
-def process_header_values(slurm_header_dict, slurm_conf, hpc):
+def process_header_values(slurm_header_dict, slurm_conf):
     """
        Process header dict values
        For maui, mem_per_cpu is None as mem is not calculated against billing
     """
-    if not slurm_header_dict[SlurmHeader.ntasks.value]:  # None, set to default
-        slurm_header_dict[SlurmHeader.ntasks.value] = DEFAULT_NTASKS
-    else:  # not None, convert str to int
-        slurm_header_dict[SlurmHeader.ntasks.value] = int(
-            slurm_header_dict[SlurmHeader.ntasks.value]
-        )
-
-    if not slurm_header_dict[SlurmHeader.cpus_per_task.value]:
-        slurm_header_dict[SlurmHeader.cpus_per_task.value] = DEFAULT_CPUS_PER_TASK
-    else:
-        slurm_header_dict[SlurmHeader.cpus_per_task.value] = int(
-            slurm_header_dict[SlurmHeader.cpus_per_task.value]
-        )
-
-    if hpc == HPC.mahuika.value:
-        if not slurm_header_dict[SlurmHeader.mem_per_cpu.value]:
-            slurm_header_dict[SlurmHeader.mem_per_cpu.value] = get_default_mem_per_cpu(
-                slurm_conf
-            )
+    for header in [SlurmHeader.ntasks, SlurmHeader.cpus_per_task, SlurmHeader.ntasks_per_core]:
+        if not slurm_header_dict[header.value]:
+            slurm_header_dict[header.value] = DEFAULTS[header.value]
         else:
-            slurm_header_dict[SlurmHeader.mem_per_cpu.value] = process_mem_per_cpu(
-                slurm_header_dict[SlurmHeader.mem_per_cpu.value]
-            )
+            slurm_header_dict[header.value] = int(slurm_header_dict[header.value])
+
+    if not slurm_header_dict[SlurmHeader.mem_per_cpu.value]:
+        slurm_header_dict[SlurmHeader.mem_per_cpu.value] = get_default_mem_per_cpu(
+            slurm_conf
+        )
+    else:
+        slurm_header_dict[SlurmHeader.mem_per_cpu.value] = process_mem_per_cpu(
+            slurm_header_dict[SlurmHeader.mem_per_cpu.value]
+        )
 
     return slurm_header_dict
 
@@ -224,13 +211,26 @@ def get_hpc_conf(partition):
         slurm_conf = MAHUIKA_SLURM_CONF
         hpc = HPC.mahuika.value
     else:
-        sys.exit("{} partition does not exit")
+        sys.exit("{} partition does not exist")
     return hpc, slurm_conf
+
+
+def compare_hours_and_sbatch(requested_hours, available_hours, hpc, sl_to_sbatch, sbatch_extra_args):
+    """
+       Compare requested hour with available hours
+       and either submit the job or reject
+    """
+    if requested_hours <= available_hours:
+        cmd = "{} -M {} {} {}".format(SBATCH_BIN, hpc, sl_to_sbatch, sbatch_extra_args)
+        subprocess.call(cmd, shell=True)
+    else:
+        sys.exit("Not enough core hours left, please apply for more")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("slurm_to_sbatch", help="path to slurm file to be sbatched")
+    parser.add_argument('sbatch_extra_args', nargs='*', help="all args following xx.sl")
     parser.add_argument(
         "--core_hour_json",
         default="core_hours.json",
@@ -246,43 +246,39 @@ def main():
 
     # Get raw headers
     header_dict = process_slurm_header(args.slurm_to_sbatch)
-
+    print(header_dict)
     # Get hpc_name and path to slurm.conf
     hpc, slurm_conf = get_hpc_conf(header_dict[SlurmHeader.partition.value])
 
     # Process headers for calculation
-    header_dict = process_header_values(header_dict, slurm_conf, hpc)
-
+    header_dict = process_header_values(header_dict, slurm_conf)
+    print(header_dict)
     # Get available hours
     avai_hours = get_available_chours(
         args.core_hour_json, header_dict[SlurmHeader.account.value], args.user
     )
 
     # Calculate requested hours
-    if hpc == HPC.mahuika.value:
-        cpu_weights, mem_weights = read_slurm_conf(
-            slurm_conf, header_dict[SlurmHeader.partition.value]
-        )
-        req_hours = calculate_requested_chours(
-            cpu_weights,
-            mem_weights,
-            header_dict[SlurmHeader.mem_per_cpu.value],
-            header_dict[SlurmHeader.ntasks.value],
-            header_dict[SlurmHeader.cpus_per_task.value],
-        )
-    elif hpc == HPC.maui.value:
-        # maui only bill against total cpus, no cpu_weights or mem_weights
-        req_hours = (
-            header_dict[SlurmHeader.ntasks.value]
-            * header_dict[SlurmHeader.cpus_per_task.value]
-        )
+    cpu_weights, mem_weights = read_slurm_conf(
+        slurm_conf, header_dict[SlurmHeader.partition.value]
+    )
+    print("cpu weights {}\nmem_weights {}".format(cpu_weights, mem_weights))
+    req_hours = calculate_requested_chours(
+        cpu_weights,
+        mem_weights,
+        header_dict[SlurmHeader.mem_per_cpu.value],
+        header_dict[SlurmHeader.ntasks.value],
+        header_dict[SlurmHeader.ntasks_per_core.value],
+        header_dict[SlurmHeader.cpus_per_task.value],
+    )
 
     print("Reqested hours {}".format(req_hours))
     print("Available_hours {}".format(avai_hours))
 
     # Decide whether to submit the job or not
-    compare_hours_and_sbatch(req_hours, avai_hours, hpc, args.slurm_to_sbatch)
+    compare_hours_and_sbatch(req_hours, avai_hours, hpc, args.slurm_to_sbatch, args.sbatch_extra_args)
 
 
 if __name__ == "__main__":
     main()
+
