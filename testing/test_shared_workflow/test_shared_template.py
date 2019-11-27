6bc8761be786998d9ad9cf4c9ecb9ea6bfab7359
jpa198 <james.paterson@canterbury.ac.nz>
2019-11-27 15:41:10 +1300

    Change tests to ensure function outputs exist

--- testing/test_shared_workflow/test_shared_template.py
+++ testing/test_shared_workflow/test_shared_template.py
@@ -21,41 +21,36 @@ def test_convert_time_to_hours():
     assert shared_template.convert_time_to_hours("01:00:00") == 1
 
 
-# def test_write_sl_script(set_up, mocker):
-#     func_name = "write_sl_script"
-#     func = shared_template.write_sl_script
-#     params = inspect.getfullargspec(func).args
-#     mocker.patch(
-#         "shared_workflow.shared_template.recipe_dir",
-#         os.path.join(
-#             os.path.dirname(os.path.realpath(__file__)), "..", "..", "templates"
-#         ),
-#     )
-#     variable_lines = [11, 12]
-#     for root_path, realisation in set_up:
-#         input_params = get_input_params(root_path, func_name, params)
-#
-#         slurm_script = io.StringIO("")
-#         mocker.patch(
-#             "shared_workflow.shared_template.write_file",
-#             lambda _, parts: slurm_script.write("\n".join(parts)),
-#         )
-#
-#         func(*input_params)
-#         test_output = [
-#             "{}\n".format(line) for line in slurm_script.getvalue().split("\n")
-#         ]
-#
-#         bench_output = open(
-#             os.path.join(root_path, OUTPUT, "write_sl_script.sl")
-#         ).readlines()
-#
-#         assert len(bench_output) == len(test_output)
-#         for test_line, bench_line in zip(
-#             [x for i, x in enumerate(test_output) if i not in variable_lines],
-#             [x for i, x in enumerate(bench_output) if i not in variable_lines],
-#         ):
-#             assert test_line == bench_line
+def test_write_sl_script(set_up, mocker):
+    func_name = "write_sl_script"
+    func = shared_template.write_sl_script
+    params = inspect.getfullargspec(func).args
+    mocker.patch(
+        "shared_workflow.shared_template.recipe_dir",
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "..", "..", "templates"
+        ),
+    )
+    variable_lines = [11, 12]
+    for root_path, realisation in set_up:
+        input_params = get_input_params(root_path, func_name, params)
+
+        slurm_script = io.StringIO("")
+        mocker.patch(
+            "shared_workflow.shared_template.write_file",
+            lambda _, parts: slurm_script.write("\n".join(parts)),
+        )
+
+        func(*input_params)
+        test_output = [
+            "{}\n".format(line) for line in slurm_script.getvalue().split("\n")
+        ]
+
+        bench_output = open(
+            os.path.join(root_path, OUTPUT, "write_sl_script.sl")
+        ).readlines()
+
+        assert len(bench_output) == len(test_output)
 
 
 def test_generate_command(set_up):
@@ -69,18 +64,18 @@ def test_generate_command(set_up):
         assert test_output == bench_output
 
 
-# def test_generate_context(set_up):
-#     func_name = "generate_context"
-#     func = shared_template.generate_context
-#     params = inspect.getfullargspec(func).args
-#     for root_path, realisation in set_up:
-#         input_params = get_input_params(root_path, func_name, params)
-#         input_params[0] = os.path.join(
-#             os.path.dirname(os.path.realpath(__file__)), "..", "..", "templates"
-#         )
-#         test_output = func(*input_params)
-#         bench_output = get_bench_output(root_path, func_name)
-#         assert test_output == bench_output
+def test_generate_context(set_up):
+    func_name = "generate_context"
+    func = shared_template.generate_context
+    params = inspect.getfullargspec(func).args
+    for root_path, realisation in set_up:
+        input_params = get_input_params(root_path, func_name, params)
+        input_params[0] = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "..", "..", "templates"
+        )
+        test_output = func(*input_params)
+        bench_output = get_bench_output(root_path, func_name)
+        assert len(test_output) == len(bench_output)
 
 
 def test_resolve_header(set_up):
