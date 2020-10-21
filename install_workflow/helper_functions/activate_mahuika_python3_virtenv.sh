fb6858c3585713124cf110c5d101edc073e0e2dd
jpa198 <46693562+jpa198@users.noreply.github.com>
2020-10-21 15:36:51 +1300

    Update activate_mahuika_python3_virtenv.sh

--- install_workflow/helper_functions/activate_mahuika_python3_virtenv.sh
+++ install_workflow/helper_functions/activate_mahuika_python3_virtenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+source "${env_path}/workflow/install_workflow/helper_functions/activate_common_env.sh"
+
 # Load python3, have to do this as virtualenv points to this python
 # verions, which is not accessible without loading
 module load Python/3.6.3-gimkl-2017a
-
-source "${env_path}/workflow/install_workflow/helper_functions/activate_common_env.sh"
