0818e4aeb67e7c11a509a2681ae86d5bae5bb8f1
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2021-02-24 18:46:24 +0000

    black format

--- scripts/test_adv_IM.py
+++ scripts/test_adv_IM.py
@@ -2,7 +2,7 @@
 # checks for existance of aggregated csv
 # checks for nan/null in the csv
 # checks station in csv matches stations ran
- 
+
 
 import argparse
 import pandas as pd
@@ -14,6 +14,7 @@ from IM_calculation.Advanced_IM.runlibs_2d import check_status
 import os
 import sys
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -23,18 +24,25 @@ def parse_args():
         "adv_im_model", nargs="+", type=str, help="list of adv_IM models that ran"
     )
 
-    parser.add_argument("--components", default=[const.Components.c000.str_value, const.Components.c090.str_value], nargs="+", choices=list(const.Components.iterate_str_values()), help="list of component that ran")
+    parser.add_argument(
+        "--components",
+        default=[const.Components.c000.str_value, const.Components.c090.str_value],
+        nargs="+",
+        choices=list(const.Components.iterate_str_values()),
+        help="list of component that ran",
+    )
 
     args = parser.parse_args()
-   
-    return args 
+
+    return args
+
 
 def main(sim_dir, adv_im_model, components, exit=True):
 
     failed = []
 
     for model in adv_im_model:
-        # check if csv exist 
+        # check if csv exist
         csv_path = os.path.join(sim_dir, "IM_calc/{}.csv".format(model))
         im_dir = get_im_calc_dir(sim_dir)
         # check file exist first to prevent crash
@@ -48,11 +56,11 @@ def main(sim_dir, adv_im_model, components, exit=True):
             if df.isnull().values.any():
                 failed.append(model)
 
-            #if csv exist:
+            # if csv exist:
             # check station count, if match, skip rest of checks
             csv_station_count = len(df.station.unique())
             # glob for station folders
-            list_folders = [ x for x in os.listdir(im_dir) if os.path.isdir(x) ]
+            list_folders = [x for x in os.listdir(im_dir) if os.path.isdir(x)]
             if csv_station_count == len(list_folders):
                 # station folder count matches csv.station.count
                 continue
@@ -61,14 +69,19 @@ def main(sim_dir, adv_im_model, components, exit=True):
                     # check if folder is a station run folder
                     station_model_dir = os.path.join(station_dir, model)
                     station_name = os.path.basename(station_dir)
-                    if not os.path.isdir(station_model_dir) or ( station_name in df.station.unique() ):
+                    if not os.path.isdir(station_model_dir) or (
+                        station_name in df.station.unique()
+                    ):
                         # does not match naming for a station run, possibly created by something else, skipping
                         continue
                     else:
                         result = True
                         for comp in components:
                             component_outdir = os.path.join(station_model_dir, comp)
-                            result = check_status(component_outdir, check_fail=True) and result
+                            result = (
+                                check_status(component_outdir, check_fail=True)
+                                and result
+                            )
                         # check of "Failed"
                         # no "Failed" in all Analysis*.txt == crashed
                         # "Failed" in all Analysis*.txt == failed to converge (normal), continue
@@ -78,10 +91,13 @@ def main(sim_dir, adv_im_model, components, exit=True):
                             failed.append(model)
     if len(failed) != 0:
         print("some runs have failed,. models: {}".format(failed))
-        if exit: sys.exit(1)
+        if exit:
+            sys.exit(1)
     else:
         print("check passed")
-        if exit: sys.exit(0)
+        if exit:
+            sys.exit(0)
+
 
 if __name__ == "__main__":
     args = parse_args()
