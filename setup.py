617e6faad74851706619c92e29fd5967a3ebcee6
Sung Bae <sung.bae@canterbury.ac.nz>
2021-11-17 14:32:18 +1300

    Created workflow directory

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import os
 from setuptools import setup
 
+PACKAGE_NAME = "workflow"
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-        name= "workflow",
+        name= PACKAGE_NAME,
         version = "21.11.1",
         author = "QuakeCoRE Software team",
         author_email = "",
@@ -14,7 +15,7 @@ setup(
         license = "MIT",
         keywords = "UC GM Sim workflow installation",
         url = "https://github.com/ucgmsim/slurm_gm_workflow",
-        packages = ['automation','automation.estimation','automation.lib','automation.lib.schedulers','automation.metadata','automation.submit','calculation','calculation.verification'],
+        packages = ['workflow','workflow.automation','workflow.automation.estimation','workflow.automation.lib','workflow.automation.lib.schedulers','workflow.automation.metadata','workflow.automation.submit','workflow.calculation','workflow.calculation.verification'],
         long_description=read('README.md'),
         classifiers=[
             "Development Status :: 1 - Alpha",
