caa6ebb68dcfb7bf0cdd026798be864b1eb1c070
Sung Bae <sung.bae@nesi.org.nz>
2021-11-23 00:06:22 +0000

    Added __init__.py for find_packages() to auto-pickup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 PACKAGE_NAME = "workflow"
 
@@ -18,20 +18,34 @@ setup(
     keywords="UC GM Sim workflow installation",
     url="https://github.com/ucgmsim/slurm_gm_workflow",
     #        packages = ['workflow'],
-    packages=[
-        "workflow",
-        "workflow.automation",
-        "workflow.automation.estimation",
-        "workflow.automation.lib",
-        "workflow.automation.lib.schedulers",
-        "workflow.automation.metadata",
-        "workflow.automation.submit",
-        "workflow.calculation",
-        "workflow.calculation.verification",
-        "workflow.automation.install_scripts",
-        "workflow.automation.tests",
-        "workflow.automation.execution_scripts",
-    ],
+    # packages=[
+    #    "workflow",
+    #    "workflow.automation",
+    #    "workflow.automation.estimation",
+    #    "workflow.automation.lib",
+    #    "workflow.automation.lib.schedulers",
+    #    "workflow.automation.metadata",
+    #    "workflow.automation.submit",
+    #    "workflow.calculation",
+    #    "workflow.calculation.verification",
+    #    "workflow.automation.install_scripts",
+    #    "workflow.automation.tests",
+    #    "workflow.automation.execution_scripts",
+    #    "workflow.e2e_tests",
+    # ],
+    packages=find_packages("."),  # include=['workflow','workflow.*']),
+    package_data={
+        "workflow.automation": [
+            "org/*/*.json",
+            "org/*/*.pbs",
+            "org/*/*.sl",
+            "templates/*.template",
+        ],
+        "workflow.calculation": ["gmsim_templates/*/*.yaml"],
+        "workflow.examples": ["*.yaml"],
+        "workflow.e2e_tests": ["*.yaml", "org/*/*.json"],
+        "workflow.environments": ["*.json", "org/*/*.json"],
+    },
     long_description=read("README.md"),
     classifiers=[
         "Development Status :: 1 - Alpha",
