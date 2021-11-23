df3a491bcbe0fc247757e679f6bf96bceb1c3393
Sung Bae <sung.bae@canterbury.ac.nz>
2021-11-23 16:31:14 +1300

    Fixed env_path

--- workflow/environments/helper_functions/activate_nurion_python3_virtenv.sh
+++ workflow/environments/helper_functions/activate_nurion_python3_virtenv.sh
@@ -2,4 +2,4 @@
 # Activates the specified python3 virtual environment.
 # Note: Resets the PYTHONPATH
 # Created as a separate script to allow it to be called from scripts.
-source "${env_path}/workflow/environments/helper_functions/activate_common_env.sh"
+source "${env_path}/workflow/workflow/environments/helper_functions/activate_common_env.sh"
