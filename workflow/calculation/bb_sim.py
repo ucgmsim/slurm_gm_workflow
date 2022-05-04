Changes from testing. Change output to correct units. Update paths for repo refactor. Include nt for OpenSees to read.

workflow/calculation/bb_sim.py
@@ -11,7 +11,7 @@ import numpy as np
 from qcore.siteamp_models import nt2n, cb_amp, ba18_amp, init_ba18
 from qcore import timeseries, utils
 from qcore.constants import VM_PARAMS_FILE_NAME, Components
-from scripts.site_response import run_deconvolve_and_site_response, SiteProp
+from workflow.calculation.site_specific_BB.site_response import run_deconvolve_and_site_response, SiteProp
 from workflow.automation import platform_config
 
 if __name__ == "__main__":
@@ -412,7 +412,7 @@ def main():
                 lf_c = np.hstack((lf_start_padding_ts, lf_filtered, lf_end_padding_ts))
                 bb_acc[:, j] = run_deconvolve_and_site_response(
                     hf_c + lf_c, Components(j), site_properties, dt=bb_dt, logger=logger
-                )
+                )/9.81
         else:
             logger.debug(
                 f"Station {stat.name} does not have a site specific file. Running vs30 based amplification"

workflow/calculation/site_specific_BB/site_response.py
@@ -29,7 +29,7 @@ dampS_soil = 1.0 / (2.0 * Qs)  # damping ratio - shear (dimensionless)
 dampP_soil = 1.0 / (2.0 * Qp)  # damping ratio - compression (dimensionless)
 
 SITE_AMP_SCRIPT = (
-    Path(__file__).parent / "site_specific_amplification" / "run_site_amp.tcl"
+    Path(__file__).parent / "run_site_amp.tcl"
 )
 
 
@@ -260,7 +260,7 @@ def run_deconvolve_and_site_response(
     :param component: The name of the component. Used to determine the transfer function to use
     :param site_properties: A SiteProp object for the location
     :param dt: The timestep for the given waveform
-    :return: A waveform with site specific amplification applied. Has the same shape as waveform
+    :return: A waveform with site specific amplification applied. Has the same shape as waveform and units of m/s/s
     """
     size = acceleration_waveform.size
 
@@ -313,14 +313,15 @@ def run_deconvolve_and_site_response(
             # File name doesn't matter for temp file
             np.savetxt(file_name, bbgm_decon_vel)
             params_path = td / "params.tcl"
-            site_properties.to_tcl(params_path)
+            site_properties.to_tcl(params_path, nt=size)
             try:
                 out_file = call_opensees(file_name.name, params_path, td, logger=logger)
             except RuntimeError as e:
                 logger.error(f"This didn't work: {component}, {site_properties.name}")
                 raise e
-        # Acceleration
-        out_waveform = np.loadtxt(out_file)
+            else:
+                # Acceleration in m/s/s
+                out_waveform = np.loadtxt(out_file)
 
     return out_waveform
 