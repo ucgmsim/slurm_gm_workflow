d2f28de11b27c80ecb4f0d1d9dae3024487ae5d9
James Paterson <james.paterson@canterbury.ac.nz>
2022-11-29 13:52:43 +1300

    Update dependant task to use new dependency style

--- workflow/automation/lib/MgmtDB.py
+++ workflow/automation/lib/MgmtDB.py
@@ -300,9 +300,7 @@ class MgmtDB:
         tasks = []
         for process in const.ProcessType:
             for dependency in process.dependencies:
-                if isinstance(dependency, tuple):
-                    dependency = dependency[0]
-                if entry.proc_type == dependency:
+                if entry.proc_type == dependency.process.value:
                     job_id = cur.execute(
                         "SELECT `job_id` FROM `state` WHERE proc_type = ? and status = ? and run_name = ?",
                         (process.value, const.Status.completed.value, entry.run_name),
