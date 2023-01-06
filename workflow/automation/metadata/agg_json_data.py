cae6bb4ffa3f1b0e787533ac8463ab2b0fd352b8
James Paterson <james.paterson@canterbury.ac.nz>
2023-01-06 13:34:43 +1300

    Formatting

--- workflow/automation/metadata/agg_json_data.py
+++ workflow/automation/metadata/agg_json_data.py
@@ -107,9 +107,7 @@ def convert_df(df: pd.DataFrame):
                 # Convert components to boolean
                 elif Components.has_value(meta_col):
                     df.loc[df[proc_type, meta_col].isna(), (proc_type, meta_col)] = 0.0
-                    df[(proc_type, meta_col)] = df[(proc_type, meta_col)].astype(
-                        bool
-                    )
+                    df[(proc_type, meta_col)] = df[(proc_type, meta_col)].astype(bool)
                 # status, status_1, do nothing to df, keep status string as it is
                 elif MetadataField.status.value in meta_col:
                     continue
