5bd0e11743f7b35868e7dce01ef686f23ce97822
James Paterson <james.paterson@canterbury.ac.nz>
2023-03-21 11:33:00 +1300

    Removed patching as it wasn't actually helping (Function defaults are defined at load time, so updating the variables later doesn't help, so custom values have to be passed in manually) Remove constants, include variables. Shouldn't actually matter which config set is used to run it

--- workflow/automation/tests/test_estimation/test_estimation.py
+++ workflow/automation/tests/test_estimation/test_estimation.py
@@ -1,9 +1,9 @@
 """Unitests for estimation, the aim is to
 a) ensure that the estimate_wct workflow works 
 """
+import numpy as np
 import pytest
-
-from unittest.mock import patch
+from qcore import config
 
 import workflow.automation.estimation.estimate_wct
 
@@ -88,41 +88,57 @@ def test_IM_single(data, true, tolerance):
     check_chours(chours, true, tolerance)
 
 
-PHYSICAL_NCORES_PER_NODE = 40
+# Tests based on Maui parameters
+host, host_config_path = config.determine_machine_config("maui")
+qconfig = config.get_machine_config(config_path=host_config_path)
+
+MAX_JOB_WCT = qconfig[config.ConfigKeys.MAX_JOB_WCT.name]
+MAX_NODES_PER_JOB = qconfig[config.ConfigKeys.MAX_NODES_PER_JOB.name]
+PHYSICAL_NCORES_PER_NODE = qconfig[config.ConfigKeys.cores_per_node.name]
+MAX_CH_PER_JOB = qconfig[config.ConfigKeys.MAX_CH_PER_JOB.name]
 
 
-@patch("workflow.automation.estimation.estimate_wct.MAX_JOB_WCT", 24.0)
-@patch("workflow.automation.estimation.estimate_wct.MAX_NODES_PER_JOB", 240)
-@patch(
-    "workflow.automation.estimation.estimate_wct.PHYSICAL_NCORES_PER_NODE",
-    PHYSICAL_NCORES_PER_NODE,
-)
-@patch("workflow.automation.estimation.estimate_wct.MAX_CH_PER_JOB", 1200 * 40)
 @pytest.mark.parametrize(
     ["in_params", "out_count", "out_time"],
     [
-        ((PHYSICAL_NCORES_PER_NODE * 2, 12), PHYSICAL_NCORES_PER_NODE * 2, 12),
-        ((PHYSICAL_NCORES_PER_NODE, 25), PHYSICAL_NCORES_PER_NODE * 2, 12.5),
         (
-            (PHYSICAL_NCORES_PER_NODE * 240 * 2, 12.0),
-            PHYSICAL_NCORES_PER_NODE * 240,
-            5.0,
+            (PHYSICAL_NCORES_PER_NODE * 2, MAX_JOB_WCT / 2),
+            PHYSICAL_NCORES_PER_NODE * 2,
+            MAX_JOB_WCT / 2,
         ),
-        ((PHYSICAL_NCORES_PER_NODE * 241, 25), PHYSICAL_NCORES_PER_NODE * 240, 5),
         (
-            (PHYSICAL_NCORES_PER_NODE * 239, 23),
-            PHYSICAL_NCORES_PER_NODE * 239,
-            1200 / 239,
+            (PHYSICAL_NCORES_PER_NODE, MAX_JOB_WCT + 1),
+            PHYSICAL_NCORES_PER_NODE * 2,
+            (MAX_JOB_WCT + 1) / 2,
+        ),
+        (
+            (PHYSICAL_NCORES_PER_NODE * MAX_NODES_PER_JOB * 2, MAX_JOB_WCT / 2),
+            PHYSICAL_NCORES_PER_NODE * MAX_NODES_PER_JOB,
+            MAX_CH_PER_JOB / PHYSICAL_NCORES_PER_NODE / MAX_NODES_PER_JOB,
+        ),
+        (
+            (PHYSICAL_NCORES_PER_NODE * (MAX_NODES_PER_JOB + 1), MAX_JOB_WCT + 1),
+            PHYSICAL_NCORES_PER_NODE * MAX_NODES_PER_JOB,
+            MAX_CH_PER_JOB / PHYSICAL_NCORES_PER_NODE / MAX_NODES_PER_JOB,
+        ),
+        (
+            (PHYSICAL_NCORES_PER_NODE * (MAX_NODES_PER_JOB - 1), MAX_JOB_WCT - 1),
+            PHYSICAL_NCORES_PER_NODE * (MAX_NODES_PER_JOB - 1),
+            MAX_CH_PER_JOB / PHYSICAL_NCORES_PER_NODE / (MAX_NODES_PER_JOB - 1),
         ),
     ],
 )
-def test_confine_wct_node_parameters(in_params, out_count, out_time, tolerance=0.1):
+def test_confine_wct_node_parameters(in_params, out_count, out_time):
     (
         test_count,
         test_time,
     ) = workflow.automation.estimation.estimate_wct.confine_wct_node_parameters(
-        *in_params
+        *in_params,
+        max_wct=MAX_JOB_WCT,
+        max_core_count=MAX_NODES_PER_JOB * PHYSICAL_NCORES_PER_NODE,
+        max_core_hours=MAX_CH_PER_JOB,
+        cores_per_node=PHYSICAL_NCORES_PER_NODE,
     )
 
-    check_chours(test_count, out_count, tolerance)
-    check_chours(test_time, out_time, tolerance)
+    assert np.isclose(test_count, out_count)
+    assert np.isclose(test_time, out_time)
