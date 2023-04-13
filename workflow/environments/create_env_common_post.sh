a1269ca03dac8c856e2ac3f6bf46b9af96c26f09
James <james.paterson@canterbury.ac.nz>
2023-04-13 13:35:29 +1200

    Remove post setup script as it wasn't being used and duplicates other functionality Remove deprecated module load calls Remove python module load calls as these are now provided by the shared bashrc file Add the python load functions for Nesi to the python_load_functions script to ensure they're backed up Removed a lot of unneeded comments

--- workflow/environments/create_env_common_post.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install qcore & Empirical Engine & IM_calc
-pip install -e ./qcore
-pip install -e ./Empirical_Engine
-pip install -e ./IM_calculation
-pip install -e ./visualization
