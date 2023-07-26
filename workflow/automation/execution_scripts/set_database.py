8ae6ccf9efbb1bf807875af5ab53e7c8daec048d
sungeunbae <sung.bae@canterbury.ac.nz>
2023-07-26 14:33:27 +1200

    Updated black

--- workflow/automation/execution_scripts/set_database.py
+++ workflow/automation/execution_scripts/set_database.py
@@ -57,27 +57,42 @@ def main():
         db_cur.execute("BEGIN")
         for (
             state,
-            (apply_to_all, apply_to_pattern, apply_to_not_pattern,),
+            (
+                apply_to_all,
+                apply_to_pattern,
+                apply_to_not_pattern,
+            ),
         ) in config.items():
             if len(apply_to_all) > 0:
                 db_cur.execute(
                     f"UPDATE state SET {db.col_status} = ?, last_modified = strftime('%s','now') "
                     f"WHERE proc_type IN ({', '.join('?'*len(apply_to_all))})",
-                    (state.value, *[x.value for x in apply_to_all],),
+                    (
+                        state.value,
+                        *[x.value for x in apply_to_all],
+                    ),
                 )
             if len(apply_to_pattern) > 0:
                 for pattern, task_set in apply_to_pattern:
                     db_cur.execute(
                         f"UPDATE state SET {db.col_status} = ?, last_modified = strftime('%s','now') "
                         f"WHERE run_name LIKE ? AND proc_type IN ({', '.join('?'*len(task_set))})",
-                        (state.value, pattern, *[x.value for x in task_set],),
+                        (
+                            state.value,
+                            pattern,
+                            *[x.value for x in task_set],
+                        ),
                     )
             if len(apply_to_not_pattern) > 0:
                 for pattern, task_set in apply_to_not_pattern:
                     db_cur.execute(
                         f"UPDATE state SET {db.col_status} = ?, last_modified = strftime('%s','now') "
                         f"WHERE run_name NOT LIKE ? AND proc_type IN ({', '.join('?'*len(task_set))})",
-                        (state.value, pattern, *[x.value for x in task_set],),
+                        (
+                            state.value,
+                            pattern,
+                            *[x.value for x in task_set],
+                        ),
                     )
 
 
