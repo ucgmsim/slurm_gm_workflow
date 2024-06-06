30f4034c5855f991433990b77a17702c07e04217
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2024-06-06 14:33:43 +1200

    fix tests

--- workflow/automation/tests/test_submit_scripts/test_submit_emod3d.py
+++ workflow/automation/tests/test_submit_scripts/test_submit_emod3d.py
@@ -4,8 +4,10 @@ import pytest
 from qcore.utils import load_yaml as mocked_load_yaml
 
 import workflow.automation.submit.submit_emod3d
-from workflow.automation.tests.test_common_set_up import get_fault_from_rel, set_up
-from workflow.automation.sim_params import load_sim_params as mocked_load_sim_params
+from workflow.automation.sim_params import \
+    load_sim_params as mocked_load_sim_params
+from workflow.automation.tests.test_common_set_up import (get_fault_from_rel,
+                                                          set_up)
 
 # from testing.conftest import init_scheduler
 
@@ -44,7 +46,7 @@ def test_main(set_up, mocker):
         )
         # Fault will probably change on each set of data, so reset these every time
         mocker.patch(
-            "workflow.automation.submit.submit_emod3d.utils.load_sim_params",
+            "workflow.automation.submit.submit_emod3d.sim_params.load_sim_params",
             lambda x: mocked_load_sim_params(os.path.join(rel_dir, x)),
         )
 
--- workflow/automation/tests/test_submit_scripts/test_submit_hf.py
+++ workflow/automation/tests/test_submit_scripts/test_submit_hf.py
@@ -3,8 +3,10 @@ import os
 import pytest
 
 import workflow.automation.submit.submit_hf
-from workflow.automation.tests.test_common_set_up import get_fault_from_rel, set_up
-from workflow.automation.sim_params import load_sim_params as mocked_load_sim_params
+from workflow.automation.sim_params import \
+    load_sim_params as mocked_load_sim_params
+from workflow.automation.tests.test_common_set_up import (get_fault_from_rel,
+                                                          set_up)
 
 
 @pytest.mark.usefixtures("init_scheduler")
@@ -22,7 +24,7 @@ def test_main(set_up, mocker):
         )
         # Fault will probably change on each set of data, so reset this every time
         mocker.patch(
-            "workflow.automation.submit.submit_hf.utils.load_sim_params",
+            "workflow.automation.submit.submit_hf.sim_params.load_sim_params",
             lambda x: mocked_load_sim_params(os.path.join(rel_dir, x)),
         )
 
