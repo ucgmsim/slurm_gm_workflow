bc1f67516604be8b2e790e87799fc242ddbaf49d
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2024-06-06 14:36:39 +1200

    fix test script

--- workflow/automation/tests/test_scripts/test_set_runparams.py
+++ workflow/automation/tests/test_scripts/test_set_runparams.py
@@ -3,9 +3,11 @@ from io import StringIO
 
 from qcore.utils import load_yaml as mocked_load_yaml
 
-from workflow.automation.tests.test_common_set_up import get_fault_from_rel, set_up
+from workflow.automation.sim_params import \
+    load_sim_params as mocked_load_sim_params
+from workflow.automation.tests.test_common_set_up import (get_fault_from_rel,
+                                                          set_up)
 from workflow.calculation import create_e3d
-from workflow.automation.sim_params import load_sim_params as mocked_load_sim_params
 
 EXPECTED_DATA = """all_in_one=1
 bfilt=4
@@ -112,7 +114,7 @@ def test_create_run_params(set_up, mocker):
             os.path.join(root_path, "CSRoot", "Runs", fault, x)
         )
         mocker.patch(
-            "workflow.calculation.create_e3d.utils.load_sim_params",
+            "workflow.calculation.create_e3d.sim_params.load_sim_params",
             get_mocked_sim_params,
         )
 
