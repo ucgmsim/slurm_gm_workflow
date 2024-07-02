3fe0a20a92a80bf45564a60982d590bff4ec8cb3
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2024-07-02 15:42:11 +1200

    Added total CPU hours needed for each machine

--- workflow/automation/estimation/get_est_vs_used_cputime.py
+++ workflow/automation/estimation/get_est_vs_used_cputime.py
@@ -1,14 +1,17 @@
 """
-Collect estimated vs used CPU time for Cybershake-styled runs
+Collect estimated vs used CPU time for Cybershake-styled runs on NeSI. This only works on Maui/Mahuika running SLURM,
+as it relies on "sacct" command to get the job information.
 
 This is particularly useful to accurately estimate the core hours needed for the entire set of Cybershake-styled
-simulations. One can run the MEDIAN simulations only, and then run this script to collect the estimated core hours and
+simulation. One can run the MEDIAN events only, then run this script to collect the estimated core hours and
 the cpu time actually used.
-By multiplying the time_used by the number of realisations,
-we can accurately estimate the total core hours required to run the entire set of simulations.
+By multiplying the time_used by the number of realisations, we can accurately estimate the total core hours required
+to run the entire set of simulations.
 
 This script is also useful to assess the quality of wall clock time estimation. If the estimated time is significantly
-different from the time used, it may indicate that the wall clock time estimation method needs to be improved.
+different from the time used, it may indicate that the estimation method needs to be improved.
+
+
 
 """
 
@@ -26,6 +29,8 @@ from qcore import simulation_structure
 PROCESS_TYPE = dict([(proc.value, proc.name) for proc in ProcessType])
 REVERSE_PROCESS_TYPE = {name: value for value, name in PROCESS_TYPE.items()}
 
+machines = ["maui", "mahuika"]
+MEDIAN_ONLY_PROCS = ["VM_GEN", "INSTALL_FAULT", "VM_PARAMS"]
 DEFAULT_OUTFILE = "est_vs_used_cpu_time.csv"
 CONFIG_JSON = Path(__file__).parents[1] / "org/nesi/config.json"
 
@@ -146,19 +151,20 @@ def main():
     rows = cursor.fetchall()
     jobs_from_db = {
         str(row[2]): (row[0], PROCESS_TYPE.get(row[1])) for row in rows
-    }  # job_id : (run_name, proc_type_str)
+    }  # job_id : (run_name, proc_type_str) / ensure job_id is a str
     print(f"Record found in DB : {len(jobs_from_db.keys())} entries")
 
     # Initialize a list to add the data to
     data_list = []
 
     # Read realization data from the list file
+    assert args.list.exists()
     realization_numbers = read_realization_list(args.list) if args.list.exists() else {}
 
-    # Iterate over job_ids
-    machines = ["maui", "mahuika"]
+    # initialize an empty list for each machine in machine_jobs dictionary
     machine_jobs = {machine: [] for machine in machines}
 
+    # Iterate over job_ids collected from DB
     for job_id, (run_name, proc_type_str) in jobs_from_db.items():
         assert job_id is not None, f"{run_name} {proc_type_str}"
         machine = proc_type_machine_dict[proc_type_str]
@@ -166,9 +172,23 @@ def main():
 
     for machine in machines:
         job_ids = ",".join(machine_jobs[machine])
+        # Get the job information from the SLURM database
+        # eg.
+        # $ sacct -j 4088784,4088640 --format="JobID,Elapsed,TimeLimit,AllocCPUS" -n
+        # 4088640        02:10:42   08:17:00        160
+        # 4088640.bat+   02:10:42                    80
+        # 4088640.ext+   02:10:42                   160
+        # 4088640.0      02:10:06                   160
+        # 4088784        00:03:38   00:30:00         80
+        # 4088784.bat+   00:03:38                    80
+        # 4088784.ext+   00:03:38                    80
+        # 4088784.0      00:03:19                    80
+        #
+        # The output above has jobid.{batch,extern,0} steps which are not needed.
+        # We will filter them out using ask command
         cmd = (
-            f'sacct -j {job_ids} -M {machine} --format="JobID,JobName,Elapsed,TimeLimit,AllocCPUS" -n '
-            + "|awk '{$1=$1} NF==5'"
+            f'sacct -j {job_ids} -M {machine} --format="JobID,Elapsed,TimeLimit,AllocCPUS" -n '
+            + "|awk '{$1=$1} NF==4'"  # squash whitespaces and select lines with 4 fields
         )
         sacct_output = subprocess.check_output(
             cmd,
@@ -177,19 +197,21 @@ def main():
         sacct_lines = sacct_output.decode().splitlines()
         assert len(sacct_lines) == len(
             machine_jobs[machine]
-        ), f"Something is wrong:  {len(sacct_lines)} {len(machine_jobs[machine])} {cmd}"  # make sure the command returned the output we want
+        ), f": {cmd}\nReturned incorrect number of records {len(sacct_lines)}, expected {len(machine_jobs[machine])}"
         for line in sacct_lines:
             job_info = line.split()
-            assert len(job_info) == 5
-            job_id, _, time_used, time_requested, num_cpus = job_info
-            assert job_id in machine_jobs[machine]
+            assert len(job_info) == 4, f"Should have exactly 4 fields: {line}"
+            job_id, time_used, time_requested, num_cpus = job_info
+            assert (
+                job_id in machine_jobs[machine]
+            ), f"Job ID {job_id} not found in the list of jobs for {machine}"
             run_name, proc_type_str = jobs_from_db[job_id]
             # Determine if it's a MEDIAN event (based on run_name)
             is_median_event = "_REL" not in run_name  # otherwise, it is a realization
 
             num_rels = realization_numbers.get(run_name, 0) if is_median_event else 0
             # we don't need to consider realisations for VM_GEN or INSTALL_FAULT
-            num_rels = 0 if proc_type_str in ["VM_GEN", "INSTALL_FAULT"] else num_rels
+            num_rels = 0 if proc_type_str in MEDIAN_ONLY_PROCS else num_rels
 
             # Convert time_used time to seconds
             time_used_seconds = convert_time_used_to_seconds(time_used)
@@ -208,17 +230,23 @@ def main():
                     "time_used": time_used,
                     "cpu_seconds_used": cpu_seconds_used,
                     "num_rels": num_rels,
-                    "cpu_seconds_need_for_all_rels": cpu_seconds_used*num_rels
+                    "cpu_seconds_need_for_all_rels": cpu_seconds_used * num_rels,
                 }
             )
 
     # Create a DataFrame from the list of dictionaries
     df = pd.DataFrame(data_list)
-    df.sort_values(['run_name','proc_type'], inplace=True)
+    df.sort_values(["run_name", "proc_type"], inplace=True)
     # Write to a CSV file
     df.to_csv(args.outfile, index=False)
 
     print(f"CSV file {args.outfile} created successfully!")
+    for machine in machines:
+        machine_df = df[df["machine"] == machine]
+        total = machine_df["cpu_seconds_need_for_all_rels"].sum()
+        print(
+            f"Total CPU hours needed for all realisations on {machine} : {total/3600:.2f} hours"
+        )
 
 
 if __name__ == "__main__":
