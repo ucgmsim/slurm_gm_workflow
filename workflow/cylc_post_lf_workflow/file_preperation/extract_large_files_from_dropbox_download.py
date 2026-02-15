1f71781d03d4a6a4418cde726cd0a06347d424ca
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2026-02-16 11:24:57 +1300

    debug v20p10 source extraction

--- workflow/cylc_post_lf_workflow/file_preperation/extract_large_files_from_dropbox_download.py
+++ workflow/cylc_post_lf_workflow/file_preperation/extract_large_files_from_dropbox_download.py
@@ -271,17 +271,26 @@ def main():
     
     print(f"Processing version: {args.version}, fault: {args.fault}")
     
-    print(f"Move and extract LF dir for fault {args.fault}...")    
-    process_directory_tree(
-        tar_original_setup_files_from_dropbox / "LF" / args.fault,
-        extracted_original_setup_files_from_dropbox / "LF" / args.fault
-    )
+    # print(f"Move and extract LF dir for fault {args.fault}...")    
+    # process_directory_tree(
+    #     tar_original_setup_files_from_dropbox / "LF" / args.fault,
+    #     extracted_original_setup_files_from_dropbox / "LF" / args.fault
+    # )
 
     print(f"Move and extract Sources dir for fault {args.fault}...")
-    process_directory_tree(
-        tar_original_setup_files_from_dropbox / "Sources" / f"{args.fault}.tar",
-        extracted_original_setup_files_from_dropbox / "Sources" / f"{args.fault}"
-    )
+    if args.version == "v25p10":
+        process_directory_tree(
+            tar_original_setup_files_from_dropbox / "Sources" / f"{args.fault}.tar",
+            extracted_original_setup_files_from_dropbox / "Sources" / f"{args.fault}"
+        )
+        
+    elif args.version == "v25p11":
+        process_directory_tree(
+            tar_original_setup_files_from_dropbox / "Sources" / f"{args.fault}.tar",
+            extracted_original_setup_files_from_dropbox / "Sources" / f"{args.fault}"
+        )
+    else:
+        raise ValueError(f"Unsupported version: {args.version}")
 
     # print(f"Move VMs/HDF5 file for fault {args.fault}...")
     # dest_vm_dir = extracted_original_setup_files_from_dropbox / "VMs" / "HDF5"
